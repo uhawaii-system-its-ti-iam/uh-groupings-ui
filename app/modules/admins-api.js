95071b6ce4610202bc5894060961fa3f8e656155
Kevin Lanni <therealklanni@gmail.com>
2015-05-07 13:34:42 -0700

    add postRemoveAdmin mock in admins-api module

--- app/modules/admins-api.js
+++ app/modules/admins-api.js
@@ -5,33 +5,47 @@ module.exports = function (App) {
   var $ = require('jquery');
 
   App.module('AdminsApi', function (AdminsApi, Core) {
-    var dfd = $.Deferred();
+    var adminCollection = new UserCollection([
+      {
+        id: 1,
+        name: 'David Banner',
+        isAdmin: true
+      },
+      {
+        id: 2,
+        name: 'Johnny Storm',
+        isAdmin: true
+      },
+      {
+        id: 3,
+        name: 'Bruce Wayne',
+        isAdmin: true
+      },
+      {
+        id: 4,
+        name: 'Clark Kent',
+        isSuperUser: true
+      }
+    ]);
 
     var API = {
       getAdminsEntities: function () {
-        dfd.resolve(new UserCollection([
-          {
-            name: 'David Banner',
-            isAdmin: true
-          },
-          {
-            name: 'Johnny Storm',
-            isAdmin: true
-          },
-          {
-            name: 'Bruce Wayne',
-            isAdmin: true
-          },
-          {
-            name: 'Clark Kent',
-            isSuperUser: true
-          }
-        ]));
+        var dfd = $.Deferred();
+
+        dfd.resolve(adminCollection);
+
+        return dfd.promise();
+      },
+      postRemoveAdmin: function (model) {
+        var dfd = $.Deferred();
+
+        dfd.resolve(adminCollection.remove(model));
 
         return dfd.promise();
       }
     };
 
     Core.reqres.setHandler('admins:entities', API.getAdminsEntities);
+    Core.reqres.setHandler('admins:remove', API.postRemoveAdmin);
   });
 };
