87072e04a1bcc4fbd7105a46083528b295a76cc1
Kevin Lanni <therealklanni@gmail.com>
2015-05-12 12:15:35 -0700

    add table sorting to groups view

--- app/views/groups.js
+++ app/views/groups.js
@@ -3,11 +3,28 @@
 var Backbone = require('backbone');
 var GroupView = require('../views/group');
 var groupsTmpl = require('../../templates/groups.hbs');
+var tableSorter = require('../table-sorter');
 
 module.exports = Backbone.Marionette.CompositeView.extend({
   template: groupsTmpl,
   tagName: 'div',
   className: 'col-xs-12 groups-container',
   childView: GroupView,
-  childViewContainer: 'tbody'
+  childViewContainer: 'tbody',
+
+  events: {
+    'click th': 'sortByColumn'
+  },
+
+  onRenderCollection: function () {
+    var sortKey = this.collection.state.sortKey;
+    var sortIcons = this.$el
+      .find('th')
+      .not('[data-property="' + sortKey + '"]')
+      .find('span.fa');
+
+    sortIcons.toggleClass('hidden', true);
+  },
+
+  sortByColumn: tableSorter
 });
