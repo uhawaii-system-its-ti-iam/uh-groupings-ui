ce5e01b08ef4d3def8d0468f1a005220dd16bc86
Matthew Polizzotti <mpolizzotti@unicon.net>
2015-07-06 10:14:53 -0700

    Add mock authentication & user API.

--- clientserver/middleware/index.js
+++ clientserver/middleware/index.js
@@ -4,12 +4,14 @@
 // Modules & variables.
 var favicon = require('serve-favicon'),
     morgan = require('morgan'),
+    session = require('client-sessions'),
     bodyParser = require('body-parser'),
     methodOverride = require('method-override'),
     cookieParser = require('cookie-parser'),
     express = require('express'),
     proxy = require('proxy-middleware'),
-    url = require('url');
+    url = require('url'),
+    _ = require('underscore');
 
 // Export.
 module.exports = function (environment) {
@@ -22,14 +24,24 @@ module.exports = function (environment) {
     environment.express.use(morgan('dev'));
 
     // Enable proxy.
-    environment.express.enable('trust proxy');
+    if (!_.isEmpty(environment.PROXY)) {
+        console.log('Execute.');
+        environment.express.enable('trust proxy');
+        environment.express.use(environment.PROXY_ROUTE, proxy(url.parse(environment.PROXY)));
+    }
 
-    // Proxy.
-    environment.express.use(environment.PROXY_ROUTE, proxy(url.parse(environment.PROXY)));
+    // Enable sessions.
+    environment.express.use(session({
+        cookieName: 'session',
+        secret: 'crU8rud8uWRabR8=+s@e',
+        duration: 24 * 60 * 60 * 1000,
+        activeDuration: 1000 * 60 * 5,
+        ephemeral: true
+    }));
 
     // Body parser.
-    environment.express.use(bodyParser.json());
     environment.express.use(bodyParser.urlencoded({extended: false}));
+    environment.express.use(bodyParser.json());
 
     // Method override.
     // Use HTTP verbs such as PUT or DELETE in places where the client doesn't support it.
--- clientserver/routes/index.js
+++ clientserver/routes/index.js
@@ -1,9 +1,22 @@
 /* Route middleware.
  * Map routes to separate module files.
 --------------------------------------*/
+
+// Modules & variables.
+var _ = require('underscore');
+
+// Export.
 module.exports = function (environment) {
     'use strict';
 
+    // Page.
     require('./page')(environment);
+
+    // Mock API.
+    if (_.isEmpty(environment.PROXY)) {
+        require('./authentication')(environment);
+        require('./user')(environment);
+    }
+
     return environment;
 };
