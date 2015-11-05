84fb95d08b13aa50d1ca32074749b0cd87cd0149
Bradley Gore <bgore@unicon.net>
2015-11-04 19:56:08 -0600

    Populate data-table on grouping search view, utilizing grouping service, and add appropriate i18n messages

--- sandbox/development/js/src/app/routes/grouping-search/GroupingSearchController.js
+++ sandbox/development/js/src/app/routes/grouping-search/GroupingSearchController.js
@@ -1,6 +1,7 @@
 angular.module('routes.groupingSearch.GroupingSearchViewController', [
         'stack.page-loader',
-        'stack.i18n'
+        'stack.i18n',
+        'components.groupingsService.GroupingsService'
     ])
 
     /**
@@ -14,7 +15,8 @@ angular.module('routes.groupingSearch.GroupingSearchViewController', [
     .controller('GroupingSearchViewController', [
         '$timeout',
         '$stateParams',
-        function ($timeout, $stateParams) {
+        'GroupingsService',
+        function ($timeout, $stateParams, GroupingsService) {
             'use strict';
 
             // Define.
@@ -29,14 +31,46 @@ angular.module('routes.groupingSearch.GroupingSearchViewController', [
             ctrl = this;
 
             /**
+             * Property to house result-set
+             * @type {Array}
+             */
+            ctrl.searchResults = [];
+
+            /**
+             * Property to house flags representing the current UI State
+             * @type {Object}
+             */
+            ctrl.uiState = {
+                isSearching: false,
+                hasSearched: false
+            };
+
+            /**
              * Method calls out to a service that queries back end for matching groupings
              *
              * @method onSearch
              * @param searchPhrase {String}
              */
             ctrl.onSearch = function (searchPhrase) {
-                //TODO: build/utilize service to query groupings
-                console.log('groupingSearch:search:' + searchPhrase);
+                ctrl.uiState.isSearching = true;
+                GroupingsService.query(searchPhrase).then(function (groups) {
+                    //using timeout to make sure the loading spinner displays long enough to not just be a flicker
+                    $timeout(function () {
+                        ctrl.searchResults = groups;
+                        ctrl.uiState.isSearching = false;
+                        ctrl.uiState.hasSearched = true;
+                    }, 1000);
+                });
+            };
+
+            /**
+             * Method to handle initiating the edit state of one grouping
+             *
+             * @method editGrouping
+             * @param grouping {Object}
+             */
+            ctrl.editGrouping = function (grouping) {
+                console.log('GroupingSearchCtrl:editGrouping', grouping);
             };
 
             /**
@@ -50,7 +84,7 @@ angular.module('routes.groupingSearch.GroupingSearchViewController', [
                     if (($stateParams.searchPhrase || '').length > 0) {
                         //only matters for initial setting of uhg-grouping-search search-phrase
                         ctrl.searchPhrase = $stateParams.searchPhrase;
-                        ctrl.search(ctrl.searchPhrase);
+                        ctrl.onSearch(ctrl.searchPhrase);
                     }
                     $timeout.cancel(t);
                 }, 0);
