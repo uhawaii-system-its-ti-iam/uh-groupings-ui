f95a746810268ca4242e1e6605a34821aa601ce9
Bradley Gore <bgore@unicon.net>
2015-11-04 20:30:13 -0600

    Add result-set of group memberships to membership module view

--- sandbox/development/js/src/app/routes/membership/MembershipController.js
+++ sandbox/development/js/src/app/routes/membership/MembershipController.js
@@ -1,6 +1,8 @@
 angular.module('routes.membership.MembershipController', [
     'stack.page-loader',
-    'stack.i18n'
+    'stack.i18n',
+    'stack.authentication.AuthenticationService',
+    'components.groupingsService.GroupingsService'
 ])
 
 /**
@@ -14,7 +16,9 @@ angular.module('routes.membership.MembershipController', [
 .controller('MembershipController', [
     '$timeout',
     'translate',
-    function ($timeout, translate) {
+    'AuthenticationService',
+    'GroupingsService',
+    function ($timeout, translate, AuthenticationService, GroupingsService) {
         'use strict';
 
         // Define.
@@ -29,6 +33,41 @@ angular.module('routes.membership.MembershipController', [
         membershipCtrl = this;
 
         /**
+         * Property houses flags indicating current state of the UI
+         *
+         * @property uiState
+         * @type {{isLoadingGroupings: boolean}}
+         */
+        membershipCtrl.uiState = {
+            isLoadingGroupings: true
+        };
+
+        /**
+         * Method to handle managing edit-state of groupings
+         *
+         * @method editGrouping
+         * @param grouping {Object}
+         */
+        membershipCtrl.editGrouping = function (grouping) {
+            console.log('GroupingsMembership:editGrouping', grouping);
+        };
+
+        /**
+         * Method executes loading of owned groups
+         *
+         * @method loadGroupingMemberships
+         * @private
+         */
+        function loadGroupingMemberships() {
+            AuthenticationService.getUser().then(function (user) {
+                GroupingsService.getOwnedGroups(user.username).then(function (groups) {
+                    membershipCtrl.groupingMemberships = groups;
+                    membershipCtrl.uiState.isLoadingGroupings = false;
+                });
+            });
+        }
+
+        /**
          * Method executes initialization process.
          *
          * @method initialize
@@ -36,6 +75,7 @@ angular.module('routes.membership.MembershipController', [
          */
         function initialize() {
             var t = $timeout(function () {
+                loadGroupingMemberships();
                 // Call implementations here. Timeout is needed in order
                 // for all potentially nested directives to execute.
                 $timeout.cancel(t);
