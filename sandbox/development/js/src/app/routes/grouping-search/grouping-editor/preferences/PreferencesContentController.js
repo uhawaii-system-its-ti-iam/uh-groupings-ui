882c9b7ac6d4a61f2c543402024f2993988dfe64
Matthew Polizzotti <mpolizzotti@unicon.net>
2015-12-02 09:49:09 -0700

    CLEANUP: Clean up markup, styles and javascript for preferences.

--- sandbox/development/js/src/app/routes/grouping-search/grouping-editor/preferences/PreferencesContentController.js
+++ sandbox/development/js/src/app/routes/grouping-search/grouping-editor/preferences/PreferencesContentController.js
@@ -2,32 +2,51 @@ angular.module('routes.groupingSearch.PreferencesContentController', [
     'stack.i18n',
     'components.groupingsService.GroupingsService'
 ])
+
+/**
+ * The PreferencesContentController manages view logic rendered to the
+ * preferencesContent.html template.
+ *
+ * @class PreferencesContentController
+ * @constructor
+ * @module routes.groupingSearch.PreferencesContentController
+ */
 .controller('PreferencesContentController', [
     '$scope',
     function ($scope) {
-        // define
-        var ctrl;
-
-        // alias controller
-        ctrl = this;
-
-        // main thing we'll be working with is the grouping from the parent
-        // copy it so that it makes things like switching tabs without saving easy to revert
-        // note the use of $scope service here - but it's used to access groupingEditorCtrl instead of $parent...
-        ctrl.grouping = angular.copy($scope.groupingEditorCtrl.grouping);
+        // Define.
+        var preferencesContentController;
 
         /**
-         * Method to persist the changes
+         * Property houses a reference to the controller.
+         *
+         * @property preferencesContentController
+         * @type {Object}
+         */
+        preferencesContentController = this;
+
+        /**
+         * Property houses grouping collection.
+         *
+         * @property preferencesContentController.grouping
+         * @type {Object}
+         */
+        preferencesContentController.grouping = angular.copy($scope.groupingEditorCtrl.grouping);
+
+        /**
+         * Method handler to save preferences.
          *
          * @method save
          */
-        ctrl.save = function () {
-            //TODO: Build for-realz persistence, for now just updating in memory
-            $scope.groupingEditorCtrl.grouping.options.canAddSelf = ctrl.grouping.options.canAddSelf;
-            $scope.groupingEditorCtrl.grouping.options.canRemoveSelf = ctrl.grouping.options.canRemoveSelf;
-            $scope.groupingEditorCtrl.grouping.options.includeInListServe = ctrl.grouping.options.includeInListServe;
+        preferencesContentController.save = function () {
+            // NOTE: We are just persisting in memory for demo purposes.
+            $scope.groupingEditorCtrl.grouping.options.canAddSelf = preferencesContentController.grouping.options.canAddSelf;
+            $scope.groupingEditorCtrl.grouping.options.canRemoveSelf = preferencesContentController.grouping.options.canRemoveSelf;
+            $scope.groupingEditorCtrl.grouping.options.includeInListServe = preferencesContentController.grouping.options.includeInListServe;
 
-            //set the form state back to pristine to disable the button and indicate save done
+            // Set the form state back to pristine. This will disable the
+            // Save Options button and inform the user that we have completed
+            // the save operation.
             $scope.groupingPreferencesForm.$setPristine();
         };
     }
