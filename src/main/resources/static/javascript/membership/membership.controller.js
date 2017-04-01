4e14dec1160f614d33aab33dde302491c0d32bf7
Aaron <aaronvil@hawaii.edu>
2017-03-31 14:55:03 -1000

    updated memberships page

--- src/main/resources/static/javascript/membership/membership.controller.js
+++ src/main/resources/static/javascript/membership/membership.controller.js
@@ -1,20 +1,66 @@
-(function() {
+(function () {
 
     function MembershipJsController($scope, dataProvider) {
-        var url = "groupingsIn?username=mhodges";
-        $scope.list = [];
+        var currentUser = document.getElementById("name").innerText;
+        var urlMember = "groupingsIn?username=" + currentUser;
+        var urlOptIn = "groupingsToOptInto?username=" + currentUser;
+        $scope.membersList = [];
+        $scope.optInList = []
 
-        $scope.init = function() {
-
-            dataProvider.loadData(function(d) {
-                //sorts the data by name
-
-
-                $scope.list = d;
-                console.log($scope.list);
-            }, url)
+        $scope.init = function () {
+            //Loads Data
+            $scope.getMembership(urlMember);
+            $scope.getOptIn(urlOptIn);
+            console.log($scope.optInList);
         };
+
+        $scope.getMembership = function (URL) {
+            dataProvider.loadData(function (d) {
+                $scope.membersList = d;
+                console.log($scope.membersList);
+            }, URL)
+        };
+
+        $scope.getOptIn = function (URL) {
+            dataProvider.loadData(function (d) {
+                $scope.optInList = _.difference(d, $scope.membersList);
+                if($scope.optInList.length === 0)
+                {
+                    $scope.optInList = ["NO GROUPINGS TO OPT IN TO"]
+                }
+            }, URL);
+        };
+
+        $scope.optOut = function (grouping) {
+            console.log(grouping);
+            var optOutURL = "optOut?username=" + currentUser + "&grouping=" + $scope.membersList[grouping];
+            console.log(optOutURL);
+
+            dataProvider.loadData(function (d) {
+                $scope.membersList = d;
+                $scope.init();
+            }, optOutURL)
+        };
+
+        $scope.optIn = function (grouping) {
+            console.log(grouping);
+            var optInURL = "optIn?username=" + currentUser + "&grouping=" + $scope.optInList[grouping];
+            console.log(optInURL);
+        };
+
+        //Disables opt in button if there are no groupings to opt into.
+        $scope.isDisabled = function(index) {
+            var optIn = $scope.optInList[index];
+            if(optIn === "NO GROUPINGS TO OPT IN TO")
+            {
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
     }
+
     membershipApp.controller("MembershipJsController", MembershipJsController);
 
 })();
