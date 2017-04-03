cb247a38fbc3bccc860b94ef30d21d889fa6bd28
Kalani Sanidad <ksanidad@hawaii.edu>
2017-04-03 13:41:25 -1000

    add code for pagination. currently failing

--- src/main/resources/static/javascript/membership/membership.app.js
+++ src/main/resources/static/javascript/membership/membership.app.js
@@ -1,2 +1,3 @@
 'use strict';
+
 var membershipApp = angular.module("membershipApp", []);
--- src/main/resources/static/javascript/membership/membership.controller.js
+++ src/main/resources/static/javascript/membership/membership.controller.js
@@ -3,7 +3,8 @@
     function MembershipJsController($scope, dataProvider) {
         var url = "groupingsIn?username=mhodges";
         $scope.list = [];
-
+         $scope.totalItems = 3;
+        $scope.currentPage = 1;
         $scope.init = function() {
 
             dataProvider.loadData(function(d) {
@@ -11,19 +12,15 @@
 
                 $scope.list = d;
 
-                for (var i = 0; i < 10; i++){
-                  $scope.list.push("User " + i);
-                }
+                // for (var i = 0; i < 100; i++){
+                //   $scope.list.push("User " + i);
+                // }
                 var obj = JSON.stringify(d);
                 var par = JSON.parse(obj);
                 console.log(obj);
                 console.log(par);
             }, url)
         };
-
-        $(function(){
-          $('#memTab a:first').tab('show')
-        })
 
     }
     membershipApp.controller("MembershipJsController", MembershipJsController);
