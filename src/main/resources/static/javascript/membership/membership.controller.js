17a44cb935ff47fa03b0122f087ff1e9ef068b72
Aaron <aaronvil@hawaii.edu>
2017-04-13 09:02:35 -1000

    Updated api data calls and added loading spinner

--- src/main/resources/static/javascript/membership/membership.controller.js
+++ src/main/resources/static/javascript/membership/membership.controller.js
@@ -2,63 +2,63 @@
 
     function MembershipJsController($scope, dataProvider) {
         var currentUser = document.getElementById("name").innerText;
-        var urlMember = "groupingsIn?username=" + currentUser;
-        var urlOptIn = "groupingsToOptInto?username=" + currentUser;
+        var groupings = "myGroupings?username=" + currentUser;
         $scope.membersList = [];
-        $scope.optInList = []
+        $scope.optInList = [];
+        $scope.loading = true;
 
         $scope.init = function () {
             //Loads Data
-            $scope.getMembership(urlMember);
-            $scope.getOptIn(urlOptIn);
-            console.log($scope.optInList);
-        };
-
-        $scope.getMembership = function (URL) {
+            console.log("Getting data");
             dataProvider.loadData(function (d) {
-                $scope.membersList = d;
-                console.log($scope.membersList);
-            }, URL)
-        };
-
-        $scope.getOptIn = function (URL) {
-            dataProvider.loadData(function (d) {
-                $scope.optInList = _.difference(d, $scope.membersList);
+                $scope.membersList = d.groupingsIn;
+                $scope.optOutList = d.groupingsToOptOutOf;
+                $scope.optInList = d.groupingsToOptInTo;
                 if($scope.optInList.length === 0)
                 {
-                    $scope.optInList = ["NO GROUPINGS TO OPT IN TO"]
+                    $scope.optInList.push({'name': "NO GROUPINGS TO OPT IN TO"});
                 }
-            }, URL);
+                $scope.loading = false;
+                console.log($scope.optOutList);
+            }, groupings);
+            console.log("Data retrieved");
         };
 
+
+        // Adds user to the exclude group.
         $scope.optOut = function (grouping) {
             console.log(grouping);
-            var optOutURL = "optOut?username=" + currentUser + "&grouping=" + $scope.membersList[grouping];
+            var optOutURL = "optOut?username=" + currentUser + "&grouping=" + $scope.membersList[grouping].path;
+            var cancelOptIn = "cancelOptIn?username=" + currentUser + "&grouping=" + $scope.membersList[grouping].path;
             console.log(optOutURL);
 
             dataProvider.loadData(function (d) {
+                console.log(d);
                 $scope.membersList = d;
+                $scope.loading = true;
                 $scope.init();
             }, optOutURL)
         };
 
+        // Adds user to the include group
         $scope.optIn = function (grouping) {
             console.log(grouping);
-            var optInURL = "optIn?username=" + currentUser + "&grouping=" + $scope.optInList[grouping];
+            var optInURL = "optIn?username=" + currentUser + "&grouping=" + $scope.optInList[grouping].path;
             console.log(optInURL);
+
+            dataProvider.loadData(function (d) {
+                console.log(d);
+                $scope.membersList = d;
+                $scope.loading = true;
+                $scope.init();
+            }, optInURL)
         };
 
         //Disables opt in button if there are no groupings to opt into.
-        $scope.isDisabled = function(index) {
+        $scope.disabledOptIn = function(index) {
             var optIn = $scope.optInList[index];
-            if(optIn === "NO GROUPINGS TO OPT IN TO")
-            {
-                return true;
-            }
-            else{
-                return false;
-            }
-        }
+            return optIn.name === "NO GROUPINGS TO OPT IN TO";
+        };
     }
 
     membershipApp.controller("MembershipJsController", MembershipJsController);
