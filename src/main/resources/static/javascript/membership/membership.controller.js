9a5ccfdbf432585d2548393616084d714a1d40dc
Kalani Sanidad <ksanidad@hawaii.edu>
2017-04-12 14:48:52 -1000

    Revert "Merge branch 'dev-kalani' of https://github.com/uhawaii-system-its-ti-iam/uhgroupings into dev-kalani"
    This reverts commit ed6354355f4a1cd490fb287ab026dbad687ba01c.

--- src/main/resources/static/javascript/membership/membership.controller.js
+++ src/main/resources/static/javascript/membership/membership.controller.js
@@ -3,8 +3,6 @@
     function MembershipJsController($scope, dataProvider) {
         var currentUser = document.getElementById("name").innerText;
         var urlMember = "groupingsIn?username=" + currentUser;
-        console.log(urlMember);
-        console.log(currentUser);
         var urlOptIn = "groupingsToOptInto?username=" + currentUser;
         $scope.membersList = [];
         $scope.optInList = []
@@ -12,38 +10,31 @@
         $scope.init = function () {
             //Loads Data
             $scope.getMembership(urlMember);
-            //console.log($scope.getMembership);
             $scope.getOptIn(urlOptIn);
-            //console.log($scope.optInList);
+            console.log($scope.optInList);
         };
 
         $scope.getMembership = function (URL) {
-          var members = $scope.membersList;
-          for (var i = 0; i < 10; i++){
-            members.push("User "+i);
-          }
-            // dataProvider.loadData(function (d) {
-            //   console.log("Am i getting in here?");
-            //     $scope.membersList = d;
-            //     // console.log($scope.membersList);
-            // }, URL)
+            dataProvider.loadData(function (d) {
+                $scope.membersList = d;
+                console.log($scope.membersList);
+            }, URL)
         };
 
         $scope.getOptIn = function (URL) {
-            // dataProvider.loadData(function (d) {
-            //   console.log("My hero academia is a pretty gee anime");
-            //     $scope.optInList = _.difference(d, $scope.membersList);
-            //     if($scope.optInList.length === 0)
-            //     {
-            //         $scope.optInList = ["NO GROUPINGS TO OPT IN TO"]
-            //     }
-            // }, URL);
+            dataProvider.loadData(function (d) {
+                $scope.optInList = _.difference(d, $scope.membersList);
+                if($scope.optInList.length === 0)
+                {
+                    $scope.optInList = ["NO GROUPINGS TO OPT IN TO"]
+                }
+            }, URL);
         };
 
         $scope.optOut = function (grouping) {
-            // console.log(grouping);
+            console.log(grouping);
             var optOutURL = "optOut?username=" + currentUser + "&grouping=" + $scope.membersList[grouping];
-            // console.log(optOutURL);
+            console.log(optOutURL);
 
             dataProvider.loadData(function (d) {
                 $scope.membersList = d;
@@ -52,9 +43,9 @@
         };
 
         $scope.optIn = function (grouping) {
-            // console.log(grouping);
+            console.log(grouping);
             var optInURL = "optIn?username=" + currentUser + "&grouping=" + $scope.optInList[grouping];
-            // console.log(optInURL);
+            console.log(optInURL);
         };
 
         //Disables opt in button if there are no groupings to opt into.
