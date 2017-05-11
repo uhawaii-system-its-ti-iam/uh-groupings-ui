096d4e425ea8893ee9c7417429b9135ba4d845e4
Aaron <aaronvil@hawaii.edu>
2017-05-10 16:24:20 -1000

    updated membership page to include cancel opt in and out tables

--- src/main/resources/static/javascript/membership/membership.controller.js
+++ src/main/resources/static/javascript/membership/membership.controller.js
@@ -5,6 +5,9 @@
         var groupings = "api/groupings/" + currentUser + "/myGroupings";
         $scope.membersList = [];
         $scope.optInList = [];
+        $scope.optOutList = [];
+        $scope.optedIn = [];
+        $scope.optedOut = [];
         $scope.loading = true;
 
         $scope.totalItems = 12;
@@ -13,53 +16,78 @@
         $scope.init = function () {
             //Loads Data
             dataProvider.loadData(function (d) {
+                console.log(d);
                 $scope.membersList = d.groupingsIn;
                 $scope.optOutList = d.groupingsToOptOutOf;
                 $scope.optInList = d.groupingsToOptInTo;
+                $scope.optedIn = d.groupingsOptedInTo;
+                $scope.optedOut = d.groupingsOptedOutOf;
+                console.log($scope.optedIn.length);
+                if($scope.optedIn.length === 0)
+                {
+                    $scope.optedIn.push({'name': "NO GROUPINGS TO OPT IN TO"});
+                }
+                if($scope.optedOut.length === 0)
+                {
+                    $scope.optedOut.push({'name': "NO GROUPINGS TO OPT IN TO"});
+                }
                 if($scope.optInList.length === 0)
                 {
                     $scope.optInList.push({'name': "NO GROUPINGS TO OPT IN TO"});
                 }
                 $scope.loading = false;
-                console.log($scope.optOutList);
+                console.log($scope.optedIn);
+                console.log($scope.optedOut);
             }, groupings);
         };
 
 
         // Adds user to the exclude group.
         $scope.optOut = function (grouping) {
-            console.log(grouping);
-            var optOutURL = "optOut?username=" + currentUser + "&grouping=" + $scope.membersList[grouping].path;
-            var cancelOptIn = "cancelOptIn?username=" + currentUser + "&grouping=" + $scope.membersList[grouping].path;
+            var optOutURL = "api/groupings/" +  $scope.membersList[grouping].path + "/" + currentUser + "/optOut";
             console.log(optOutURL);
-
-            dataProvider.loadData(function (d) {
+            /*dataProvider.loadData(function (d) {
                 console.log(d);
                 $scope.membersList = d;
                 $scope.loading = true;
                 $scope.init();
-            }, optOutURL)
+            }, optOutURL)*/
         };
 
         // Adds user to the include group
         $scope.optIn = function (grouping) {
-            console.log(grouping);
-            var optInURL = "optIn?username=" + currentUser + "&grouping=" + $scope.optInList[grouping].path;
+            var optInURL = "api/groupings/" +  $scope.optInList[grouping].path + "/" + currentUser + "/optIn";
             console.log(optInURL);
 
-            dataProvider.loadData(function (d) {
+            /*dataProvider.loadData(function (d) {
                 console.log(d);
                 $scope.membersList = d;
                 $scope.loading = true;
                 $scope.init();
-            }, optInURL)
+            }, optInURL)*/
         };
 
         //Disables opt in button if there are no groupings to opt into.
         $scope.disabledOptIn = function(index) {
             var optIn = $scope.optInList[index];
-            return optIn.name === "NO GROUPINGS TO OPT IN TO";
+            var optedOut = $scope.optedOut[index];
+            return optIn.name === "NO GROUPINGS TO OPT IN TO" || optedOut.name ===  "NO GROUPINGS TO OPT IN TO";
         };
+
+        /*$scope.disableOptOut = function(index) {
+            var i = $scope.membersList;
+            var j =  $scope.optOutList;
+
+            for(var k = 0; k < j.length; k++)
+            {
+                for(var l = 0; l < i.length; l++)
+                {
+                    if (i[l].name === j[k].name){
+                        console.log(i[l]);
+                    }
+                }
+            }
+        };*/
     }
 
     membershipApp.controller("MembershipJsController", MembershipJsController);
