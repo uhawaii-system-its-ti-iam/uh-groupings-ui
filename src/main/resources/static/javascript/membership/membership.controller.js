cb5a455c1dcf09dbcf2c0c0ce2d7c76290faffdb
Kalani Sanidad <ksanidad@hawaii.edu>
2017-05-30 11:01:11 -1000

    add semiworking pagination. to delete others

--- src/main/resources/static/javascript/membership/membership.controller.js
+++ src/main/resources/static/javascript/membership/membership.controller.js
@@ -7,34 +7,40 @@
         $scope.optInList = [];
         $scope.loading = true;
 
-        $scope.totalItems = 12;
-        $scope.currentPage = 1;
+        $scope.pagedItems=[];
+        $scope.gap=5;
+        $scope.itemsPerPage = 5;
+        $scope.currentPage = 0;
 
         $scope.init = function () {
             //Loads Data
             dataProvider.loadData(function (d) {
                 $scope.membersList = d.groupingsIn;
+                for(var i = 0 ; i < 10;i++){
+                    $scope.membersList.push({name:"Group "+i});
+                }
                 $scope.optOutList = d.groupingsToOptOutOf;
                 $scope.optInList = d.groupingsToOptInTo;
+                $scope.grouptToPages();
                 if($scope.optInList.length === 0)
                 {
                     $scope.optInList.push({'name': "NO GROUPINGS TO OPT IN TO"});
                 }
                 $scope.loading = false;
-                console.log($scope.optOutList);
+                // console.log($scope.optOutList);
             }, groupings);
         };
 
 
         // Adds user to the exclude group.
         $scope.optOut = function (grouping) {
-            console.log(grouping);
+            // console.log(grouping);
             var optOutURL = "optOut?username=" + currentUser + "&grouping=" + $scope.membersList[grouping].path;
             var cancelOptIn = "cancelOptIn?username=" + currentUser + "&grouping=" + $scope.membersList[grouping].path;
-            console.log(optOutURL);
+            // console.log(optOutURL);
 
             dataProvider.loadData(function (d) {
-                console.log(d);
+                // console.log(d);
                 $scope.membersList = d;
                 $scope.loading = true;
                 $scope.init();
@@ -43,12 +49,12 @@
 
         // Adds user to the include group
         $scope.optIn = function (grouping) {
-            console.log(grouping);
+            // console.log(grouping);
             var optInURL = "optIn?username=" + currentUser + "&grouping=" + $scope.optInList[grouping].path;
-            console.log(optInURL);
+            // console.log(optInURL);
 
             dataProvider.loadData(function (d) {
-                console.log(d);
+                // console.log(d);
                 $scope.membersList = d;
                 $scope.loading = true;
                 $scope.init();
@@ -59,6 +65,56 @@
         $scope.disabledOptIn = function(index) {
             var optIn = $scope.optInList[index];
             return optIn.name === "NO GROUPINGS TO OPT IN TO";
+        };
+
+
+        //handes the groups of stuff on the pages.
+        $scope.grouptToPages=function(){
+            $scope.pagedItems=[];
+            //console.log($scope.membersList);
+            for(var i = 0; i < $scope.membersList.length ; i++){
+                console.log($scope.membersList[i]);
+                if(i % $scope.itemsPerPage === 0){
+
+                    $scope.pagedItems[Math.floor(i/$scope.itemsPerPage)] = [ $scope.membersList[i]];
+
+                }else{
+                    $scope.pagedItems[Math.floor(i/$scope.itemsPerPage)].push( $scope.membersList[i]);
+                }
+            }
+
+        };
+
+        $scope.range = function (size,start, end) {
+            var ret = [];
+            if (size < end) {
+                end = size;
+                start = size-$scope.gap;
+            }
+            if(start < 0 ){
+                start = 0;
+            }
+            for (var i = start; i < end; i++) {
+                ret.push(i);
+            }
+            //  console.log(ret);
+            return ret;
+        };
+
+        $scope.prevPage = function () {
+            if ($scope.currentPage > 0) {
+                $scope.currentPage--;
+            }
+        };
+
+        $scope.nextPage = function () {
+            if ($scope.currentPage < $scope.pagedItems.length - 1) {
+                $scope.currentPage++;
+            }
+        };
+
+        $scope.setPage = function () {
+            $scope.currentPage = this.n;
         };
     }
 
