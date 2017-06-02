1284cbea29a91d4d72acd2108ca502c9c0300a71
Aaron <aaronvil@hawaii.edu>
2017-06-01 18:00:55 -1000

    Started adding documentations for functions

--- src/main/resources/static/javascript/admin/admin.app.js
+++ src/main/resources/static/javascript/admin/admin.app.js
@@ -1,2 +1,5 @@
+/**
+ * Main app that initializes the angular controller.
+ */
 'use strict';
 var adminApp = angular.module("adminApp", []);
--- src/main/resources/static/javascript/admin/admin.controller.js
+++ src/main/resources/static/javascript/admin/admin.controller.js
@@ -1,46 +1,68 @@
 (function () {
+    /**
+     * Admin controller for the entire admin page.
+     *
+     * @param $scope        : Binding variable between controller and html page.
+     * @param dataProvider  : service function that acts as the AJAX get.
+     * @param dataUpdater   : service function that acts as AJAX post, used mainly for adding or updating
+     * @param dataDelete    : service function that acts as AJAX psst, use function mainly for delete function.
+     * @constructor
+     */
     function AdminJsController($scope, dataProvider, dataUpdater, dataDelete) {
         var currentUser = document.getElementById("name").innerText;
         var url = "api/groupings/hawaii.edu:custom:test:aaronvil:aaronvil-test/" + currentUser + "/grouping";
         $scope.list = [];
+        //Adds the loading spinner
         $scope.loading = true;
 
+        /**
+         * Initializing function for the admin page.
+         * Calls dataProvider service to retrieve a list of admins.
+         */
         $scope.init = function () {
-            setTimeout(function () {
-                dataProvider.loadData(function (d) {
-                    var temp = d.basisPlusIncludeMinusExclude.members;
+            dataProvider.loadData(function (d) {
+                var tempList = d.basisPlusIncludeMinusExclude.members;
 
-                    for (var k = 0; k < temp.length; k++) {
-                        temp[k].basis = "\u2716";
-                    }
+                //sorts the data by name
+                tempList.sort(function (a, b) {
+                    var nameA = a.name.toLowerCase(), nameB = b.name.toLowerCase();
+                    if (nameA < nameB) //sort string ascending
+                        return -1;
+                    if (nameA > nameB)
+                        return 1;
+                    return 0
+                });
 
-                    //sorts the data by name
-                    temp.sort(function (a, b) {
-                        var nameA = a.name.toLowerCase(), nameB = b.name.toLowerCase();
-                        if (nameA < nameB) //sort string ascending
-                            return -1;
-                        if (nameA > nameB)
-                            return 1;
-                        return 0
-                    });
-                    var basis = d.basis.members;
+                var basis = d.basis.members;
 
-                    for (var l = 0; l < basis.length; l++) {
-                        for (var m = 0; m < temp.length; m++) {
-                            if (basis[l].name === temp[m].name) {
-                                temp[m].basis = "\u2714";
-                            }
+                //Default add everyone as not in basis
+                for (var k = 0; k < tempList.length; k++) {
+                    tempList[k].basis = "\u2716";
+                }
+
+                //Adds whether or not a member is in the basis group or not
+                for (var l = 0; l < basis.length; l++) {
+                    for (var m = 0; m < tempList.length; m++) {
+                        if (basis[l].name === tempList[m].name) {
+                            tempList[m].basis = "\u2714";
                         }
                     }
-                    $scope.list = temp;
-                    $scope.loading = false;
-                    console.log($scope.list);
-                }, url)
-            }, 2000);
+                }
+
+                $scope.list = tempList;
+                $scope.loading = false;
+                console.log($scope.list);
+            }, url);
         };
 
+        /**
+         * Adds function that adds an member to the admin grouping.
+         * Uses dataUpdater service to post the user that is being added.
+         * If user is successfully added, dataProvider will return a Success in result Code.
+         * Else if resultCode is undefined, then user was not successfully added.
+         */
         $scope.add = function () {
-            var addUrl  = "api/groupings/hawaii.edu:custom:test:aaronvil:aaronvil-test/" + currentUser + "/" + $scope.username + "/addMemberToIncludeGroup";
+            var addUrl = "api/groupings/hawaii.edu:custom:test:aaronvil:aaronvil-test/" + currentUser + "/" + $scope.username + "/addMemberToIncludeGroup";
             $scope.testdata = [];
 
             if (confirm("You are adding " + $scope.username + " to the include list of this grouping")) {
@@ -59,13 +81,19 @@
             }
         };
 
-        $scope.remove = function (row) {
-            var deleteUser = $scope.list[row].username;
+        /**
+         * Remove function uses dataDelete Service to remove user from admin grouping.
+         * Will not delete admin if there is only one admin in the list.
+         *
+         * @param index, the index of the user based on the html table.
+         */
+        $scope.remove = function (index) {
+            var deleteUser = $scope.list[index].username;
             var deleteUrl = "api/groupings/hawaii.edu:custom:test:aaronvil:aaronvil-test/" + currentUser + "/" + deleteUser + "/deleteMemberFromIncludeGroup";
-            console.log(deleteUser);
+
             if ($scope.list.length > 1) {
-                dataDelete.deleteData(function(d) {
-                    $scope.list.splice(row, 1);
+                dataDelete.deleteData(function (d) {
+                    $scope.list.splice(index, 1);
                     $scope.init();
                 }, deleteUrl);
             }
