4c749132bb1f376b2932c56c94f9ccfe1d1d622c
Kalani Sanidad <ksanidad@hawaii.edu>
2017-06-16 15:15:48 -1000

    modified the test

--- src/test/javascript/membership.controller.test.js
+++ src/test/javascript/membership.controller.test.js
@@ -14,5 +14,27 @@ describe("MembershipJsController", function() {
         });
     }));
 
-    
-})
+    it("checkInitFunction", function() {
+        spyOn(scope, "loadData").and.callFake(function() {
+            for(var i = 1; i <= 100 ; i++){
+            scope.membersList.push({
+                "name": "Member "+ i
+            });
+            }
+        });
+
+        /**
+        expected it to be somewhat defined before calling the init
+        **/
+        expect(controller).toBeDefined();
+        expect(membersList).toBeDefined();
+
+
+        /**
+        calling the init function to start the testing
+        **/
+        scope.init();
+        
+        expect(scope.loadData).toHaveBeenCalled();
+    });
+});
