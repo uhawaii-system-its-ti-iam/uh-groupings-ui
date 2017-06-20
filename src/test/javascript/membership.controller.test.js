47d8b9b94b3da09573c209035a1b0370df994c0e
Kalani Sanidad <ksanidad@hawaii.edu>
2017-06-20 13:19:54 -1000

    fix membership testing. need to add more

--- src/test/javascript/membership.controller.test.js
+++ src/test/javascript/membership.controller.test.js
@@ -3,12 +3,15 @@ describe("MembershipJsController", function () {
     beforeEach(module('membershipApp'));
 
     var scope;
+    var testing;
     var controller;
     var dataProvider;
     var dataUpdater;
 
     beforeEach(inject(function ($rootScope, $controller, dataProvider, dataUpdater) {
+        testing = false;
         scope = $rootScope.$new();
+        // expect(scope).toBeDefined();
         controller = $controller('MembershipJsController', {
             $scope: scope,
             dataProvider: dataProvider,
@@ -17,13 +20,12 @@ describe("MembershipJsController", function () {
     }));
 
     it("checkInitFunction", function () {
-        // spyOn(scope, "init").and.callFake(function () {
-        //     scope.membersList.push({
-        //         "name": "ksanidad-test",
-        //     });
-        // });
+        spyOn(scope, "init").and.callFake(function () {
+            scope.membersList.push({
+                "name": "ksanidad-test",
+            });
+        });
         expect(controller).toBeDefined();
-        // expect(scope.membersList).toBeDefined();
     });
 
 });
