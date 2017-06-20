cbdc8390a4f5091a1c43bd9d904c15b3d18e813e
Kalani Sanidad <ksanidad@hawaii.edu>
2017-06-19 14:32:52 -1000

    fixed pom file

--- src/test/javascript/membership.controller.test.js
+++ src/test/javascript/membership.controller.test.js
@@ -1,24 +1,36 @@
-describe("MembershipJsController", function() {
+describe("MembershipJsController", function () {
 
-     beforeEach(module('membershipApp'));
+    beforeEach(module('membershipApp'));
 
     var scope;
     var controller;
     var dataProvider;
-    var dataUpdater;
-    var e;
+    var dataDelete;
+    var dataUpdater
 
-    beforeEach(inject(function($rootScope, $controller, dataProvider, dataUpdater) {
+    beforeEach(inject(function ($rootScope, $controller, dataProvider, dataDelete, dataUpdater) {
         scope = $rootScope.$new();
         controller = $controller('MembershipJsController', {
             $scope: scope,
             dataProvider: dataProvider,
+            deleteData: dataDelete,
             dataUpdater: dataUpdater
         });
     }));
 
-   it("checkInitFunction", function() {
-       e =true;
-       expect(e).toBe(true);
-    });
+    // it("checkInitFunction", function () {
+    //
+    //     var dummyElement = document.createElement('span');
+    //     document.getElementById = jasmine.createSpy('HTML Element').and.returnValue(dummyElement);
+    //
+    //     spyOn(scope, "init").and.callFake(function () {
+    //         scope.list.push({
+    //             "basis": "\u2716",
+    //             "name": "Jake Nal",
+    //             "uuid": 1021521,
+    //             "username": "jnal"
+    //         });
+    //     });
+    // });
+
 });
