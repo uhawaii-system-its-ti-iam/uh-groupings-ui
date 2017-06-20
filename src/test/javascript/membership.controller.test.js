68d08f6586f4b50d785b0a59f0a150b8ea8531c1
Kalani Sanidad <ksanidad@hawaii.edu>
2017-06-19 15:29:58 -1000

    not defined controller

--- src/test/javascript/membership.controller.test.js
+++ src/test/javascript/membership.controller.test.js
@@ -5,32 +5,25 @@ describe("MembershipJsController", function () {
     var scope;
     var controller;
     var dataProvider;
-    var dataDelete;
-    var dataUpdater
+    var dataUpdater;
 
-    beforeEach(inject(function ($rootScope, $controller, dataProvider, dataDelete, dataUpdater) {
+    beforeEach(inject(function ($rootScope, $controller, dataProvider, dataUpdater) {
         scope = $rootScope.$new();
         controller = $controller('MembershipJsController', {
             $scope: scope,
             dataProvider: dataProvider,
-            deleteData: dataDelete,
-            dataUpdater: dataUpdater
+            dataUpdater:dataUpdater
         });
     }));
 
-    // it("checkInitFunction", function () {
-    //
-    //     var dummyElement = document.createElement('span');
-    //     document.getElementById = jasmine.createSpy('HTML Element').and.returnValue(dummyElement);
-    //
-    //     spyOn(scope, "init").and.callFake(function () {
-    //         scope.list.push({
-    //             "basis": "\u2716",
-    //             "name": "Jake Nal",
-    //             "uuid": 1021521,
-    //             "username": "jnal"
-    //         });
-    //     });
-    // });
+    it("checkInitFunction", function () {
+        // spyOn(scope, "init").and.callFake(function () {
+        //     scope.membersList.push({
+        //         "name": "ksanidad-test",
+        //     });
+        // });
+        expect(controller).toBeDefined();
+        // expect(scope.membersList).toBeDefined();
+    });
 
 });
