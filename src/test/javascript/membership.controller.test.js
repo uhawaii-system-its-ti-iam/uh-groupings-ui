64897a206688601f475656a421aefec9454d0b6a
Kalani Sanidad <ksanidad@hawaii.edu>
2017-07-13 14:46:02 -1000

    added page tests

--- src/test/javascript/membership.controller.test.js
+++ src/test/javascript/membership.controller.test.js
@@ -117,36 +117,54 @@ describe("MembershipJsController", function () {
     });
 
     it("groupToPagesTesting", function(){
-        spyOn(scope, "init").and.callFake(function() {
-            for(var i = 1; i < 101; i++) {
-                scope.optInList.push({
-                    "name": "Grouped "+i
-                });
-                scope.membersList.push({
-                    "name": "Member "+i
-                });
-                scope.optedIn.push({
-                    "name": "Opted In "+i
-                });
-                scope.optedOut.push({
-                    "name": "Opted Out "+i
-                });
-            }
-        });
+        /**
+        *  Generic placeholders
+        **/
+        var testOptInList = ["ora","ora","ora","ora","ora"];
+        var testMembersList = ["ora","ora","ora","ora","ora"];
+        var testOptedIn = ["ora","ora","ora","ora","ora"];
+        var testOptedOut = ["ora","ora","ora","ora","ora"];
+        var n = 2;
 
-        var testOptInList = [];
-        var testMembersList = [];
-        var testOptedIn = [];
-        var testOptedOut = [];
+        scope.pagedItems1 = testOptInList;
+        scope.pagedItems3 = testMembersList;
+        scope.pagedItems4 = testOptedIn;
+        scope.pagedItems5 = testOptedOut;
 
-        expect(testOptInList).toBeDefined();
-        expect(testOptInList[0]).not.toBeDefined();
-        expect(testMembersList).toBeDefined();
-        expect(testOptedOut).toBeDefined();
-        expect(testOptedIn).toBeDefined();
+        expect(testOptInList.length).toEqual(5);
+        expect(scope.pagedItems1.length).toEqual(5);
+        expect(scope.currentPageOptIn).toEqual(0);
+        expect(scope.currentPageOptOut).toEqual(0);
+        expect(scope.currentPageCancelOptIn).toEqual(0);
+        expect(scope.currentPageCancelOptOut).toEqual(0);
 
-        testOptInList = scope.groupToPages(scope.optInList, testOptInList);
-        // expect(testOptInList.length).toBe(19);
+        scope.currentPage("Page Opt In Next");
+        expect(scope.currentPageOptIn).toEqual(1);
+        scope.currentPage("Page Opt In Next");
+        scope.currentPage("Page Opt In Next");
+        expect(scope.currentPageOptIn).toEqual(3);
+        scope.currentPage("Page Opt In Prev");
+        expect(scope.currentPageOptIn).toEqual(2);
+
+        scope.currentPage("Cancel Opt In Next");
+        expect(scope.currentPageCancelOptIn).toEqual(1);
+        scope.currentPage("Cancel Opt In Next");
+        expect(scope.currentPageCancelOptIn).toEqual(2);
+        scope.currentPage("Cancel Opt In Next");
+        expect(scope.currentPageCancelOptIn).toEqual(3);
+        scope.currentPage("Cancel Opt In Next");
+        expect(scope.currentPageCancelOptIn).toEqual(4);
+        scope.currentPage("Cancel Opt In Next");
+        expect(scope.currentPageCancelOptIn).toEqual(4);
+        scope.currentPage("Cancel Opt In Prev");
+        expect(scope.currentPageCancelOptIn).toEqual(3);
+        // scope.currentPage("Cancel Opt In Prev");
+        // expect(scope.currentPageCancelOptIn).toEqual(4);
+
+        scope.currentPage("Cancel Opt Out set");
+        expect(scope.currentPageCancelOptOut).toEqual(2);
+
+
     });
 
     it("testingRangem", function(){
@@ -154,6 +172,9 @@ describe("MembershipJsController", function () {
         var testingRange = [];
 
         expect(testingRange).toBeDefined();
+        testingRange = scope.range(20,14,19);
+        expect(testingRange.length).toEqual(5);
+        expect(testingRange[0]).toEqual(14);
         testingRange = scope.range(20,15,20);
         expect(testingRange.length).toEqual(5);
         expect(testingRange[0]).toEqual(15);
@@ -161,4 +182,5 @@ describe("MembershipJsController", function () {
         expect(testingRange.length).toEqual(4);
         expect(testingRange[0]).toEqual(16);
     });
+
 });
