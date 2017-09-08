f79e17c249432b44411aa81bffde84e408293353
zknoebel <zknoebel@hawaii.edu>
2017-09-08 11:55:10 -1000

    add new config file for testing, the profile/config file we had before named test is now only for use with the UHGroupings test server application. New profile/config file is named integrationTest

--- src/test/java/edu/hawaii/its/api/controller/GroupingsRestControllerTest.java
+++ src/test/java/edu/hawaii/its/api/controller/GroupingsRestControllerTest.java
@@ -54,8 +54,6 @@ public class GroupingsRestControllerTest {
         mockMvc = webAppContextSetup(context)
                 .apply(springSecurity())
                 .build();
-
-
     }
 
     @Test
--- src/test/java/edu/hawaii/its/api/service/GroupingsServiceMockTest.java
+++ src/test/java/edu/hawaii/its/api/service/GroupingsServiceMockTest.java
@@ -15,6 +15,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
@@ -24,6 +25,7 @@ import static org.junit.Assert.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Matchers.anyString;
 
+@ActiveProfiles("integrationTest")
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {SpringBootWebApplication.class})
 public class GroupingsServiceMockTest {
