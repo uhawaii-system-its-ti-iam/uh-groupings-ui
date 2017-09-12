0eb2719753795787a84a755ac0430daabfa2fea1
zknoebel <zknoebel@hawaii.edu>
2017-09-12 09:39:56 -1000

    WIP create database

---
+++ src/test/java/edu/hawaii/its/api/type/GroupRepositoryTest.java
@@ -0,0 +1,34 @@
+package edu.hawaii.its.api.type;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertNotNull;
+
+import edu.hawaii.its.holiday.configuration.SpringBootWebApplication;
+import edu.hawaii.its.holiday.service.HolidayService;
+import edu.hawaii.its.holiday.type.Holiday;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = { SpringBootWebApplication.class })
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class GroupRepositoryTest {
+
+    @Autowired
+    private HolidayService holidayService;
+
+
+    @Test
+    public void hibernateTest() {
+        List<Holiday> holidays = holidayService.findHolidays();
+
+        assertNotNull(holidays);
+    }
+}
