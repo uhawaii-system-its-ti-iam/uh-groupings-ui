8d2d641ed4fc9078f1e10f14c01777e924542364
zknoebel <zknoebel@hawaii.edu>
2017-09-15 09:59:08 -1000

    revert

--- src/main/java/edu/hawaii/its/holiday/configuration/CachingConfig.java
+++ src/main/java/edu/hawaii/its/holiday/configuration/CachingConfig.java
@@ -20,7 +20,6 @@ public class CachingConfig {
                 new ConcurrentMapCache("default"),
                 new ConcurrentMapCache("campuses"),
                 new ConcurrentMapCache("campusesById"),
-//                new ConcurrentMapCache("groups"),
                 new ConcurrentMapCache("holidays"),
                 new ConcurrentMapCache("holidaysById"),
                 new ConcurrentMapCache("holidaysByYear"),
