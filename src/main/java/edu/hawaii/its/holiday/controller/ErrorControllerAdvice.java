1b7f9d980cb56708f8c63870769d9cfd5b2d34b5
zknoebel <zknoebel@hawaii.edu>
2017-11-22 11:56:31 -1000

    don't go to home page after errors

--- src/main/java/edu/hawaii/its/holiday/controller/ErrorControllerAdvice.java
+++ src/main/java/edu/hawaii/its/holiday/controller/ErrorControllerAdvice.java
@@ -6,8 +6,10 @@ import edu.hawaii.its.holiday.access.User;
 import edu.hawaii.its.holiday.access.UserContextService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class ErrorControllerAdvice {
@@ -16,6 +18,22 @@ public class ErrorControllerAdvice {
 
     @Autowired
     private UserContextService userContextService;
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ModelAndView handelIllegalArgumentException(IllegalArgumentException iae) {
+        String username = null;
+        User user = userContextService.getCurrentUser();
+        if (user != null) {
+            username = user.getUsername();
+        }
+        logger.error("username: " + username + "; Exception: ", iae.getCause());
+
+        ModelAndView modelAndView = new ModelAndView("/error");
+        modelAndView.addObject("errCode", 500);
+        modelAndView.addObject("errMsg", iae.getMessage());
+
+        return modelAndView;
+    }
 
     @ExceptionHandler(Exception.class)
     public String handleException(Exception ex) {
