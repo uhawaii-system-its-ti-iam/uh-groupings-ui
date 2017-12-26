d7582cf0a7ab6c24d3cb535586d263932c4efa03
Aaron <aaronvil@hawaii.edu>
2017-12-26 13:22:44 -1000

    updated owners page to be similar to admin page

--- src/main/resources/static/javascript/owner/owner.service.js
+++ src/main/resources/static/javascript/owner/owner.service.js
@@ -42,7 +42,7 @@
            addData: function(callback, url) {
                $http.post(encodeURI(url))
                    .success(callback)
-                   .error(function(data, status) {
+                   .error(callback, function(data, status) {
                        console.log('Error in dataUpdater; status: ', status);
                    })
            }
