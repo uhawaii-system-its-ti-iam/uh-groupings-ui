a44e019869e58aedacd910d34777c39605eceddb
Frank Duckart <duckart@computer.org>
2018-01-30 10:39:39 -1000

    Add QA profile to configuration.

--- src/main/java/edu/hawaii/its/groupings/configuration/AppConfigRun.java
+++ src/main/java/edu/hawaii/its/groupings/configuration/AppConfigRun.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
-@Profile(value = { "localhost", "test", "integrationTest" })
+@Profile(value = { "localhost", "test", "integrationTest", "qa" })
 @Configuration
 @ComponentScan(basePackages = "edu.hawaii.its")
 @PropertySources({
