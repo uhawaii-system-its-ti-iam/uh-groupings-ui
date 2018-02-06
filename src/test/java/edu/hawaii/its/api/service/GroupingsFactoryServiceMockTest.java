1a58121973c9a4fa184ae3f37c67205b1b06c552
zknoebel <zknoebel@hawaii.edu>
2018-02-06 13:45:18 -1000

    WIP add tests for grouperFactoryServiceImpl

--- src/test/java/edu/hawaii/its/api/service/GroupingsFactoryServiceMockTest.java
+++ src/test/java/edu/hawaii/its/api/service/GroupingsFactoryServiceMockTest.java
@@ -3,12 +3,21 @@ package edu.hawaii.its.api.service;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import edu.hawaii.its.groupings.configuration.SpringBootWebApplication;
+import edu.hawaii.its.groupings.util.Dates;
+
+import edu.internet2.middleware.grouperClient.ws.beans.WsAttributeAssignValue;
+import edu.internet2.middleware.grouperClient.ws.beans.WsGroupLookup;
+import edu.internet2.middleware.grouperClient.ws.beans.WsStemLookup;
+import edu.internet2.middleware.grouperClient.ws.beans.WsSubjectLookup;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @ActiveProfiles("localTest")
@@ -17,6 +26,7 @@ import static org.junit.Assert.assertTrue;
 @WebAppConfiguration
 public class GroupingsFactoryServiceMockTest {
 
+    GrouperFactoryServiceImpl gfs = new GrouperFactoryServiceImpl();
 
     @Test
     public void addEmptyGroupTest() {
@@ -33,32 +43,29 @@ public class GroupingsFactoryServiceMockTest {
 
     @Test
     public void makeWsSubjectLookupTest() {
-//    public WsSubjectLookup makeWsSubjectLookup(String username) {
-//        WsSubjectLookup wsSubjectLookup = new WsSubjectLookup();
-//        wsSubjectLookup.setSubjectIdentifier(username);
-//
-//        return wsSubjectLookup;
+        String username = "username";
+        WsSubjectLookup lookup = gfs.makeWsSubjectLookup(username);
+
+        assertEquals(username, lookup.getSubjectIdentifier());
     }
 
     @Test
     public void makeWsGroupLookupTest() {
-//    public WsGroupLookup makeWsGroupLookup(String group) {
-//        WsGroupLookup groupLookup = new WsGroupLookup();
-//        groupLookup.setGroupName(group);
-//
-//        return groupLookup;
+        String groupPath = "path:to:group";
+        WsGroupLookup groupLookup = gfs.makeWsGroupLookup(groupPath);
+        assertEquals(groupPath, groupLookup.getGroupName());
     }
 
     @Test
     public void makeWsStemLookupTest() {
-//    public WsStemLookup makeWsStemLookup(String stemName) {
-//        return makeWsStemLookup(stemName, null);
-    }
+        String stemPath = "path:to:stem";
+        String stemUuid = "12345";
+        WsStemLookup stemLookup = gfs.makeWsStemLookup(stemPath);
+        assertEquals(stemPath, stemLookup.getStemName());
 
-    @Test
-    public void makeWsStemLookupWithUuidTest() {
-//    public WsStemLookup makeWsStemLookup(String stemName, String stemUuid) {
-//        return new WsStemLookup(stemName, stemUuid);
+        stemLookup = gfs.makeWsStemLookup(stemPath, stemUuid);
+        assertEquals(stemPath, stemLookup.getStemName());
+        assertEquals(stemUuid, stemLookup.getUuid());
     }
 
     @Test
@@ -74,12 +81,10 @@ public class GroupingsFactoryServiceMockTest {
 
     @Test
     public void makeWsAttributeAssignValueTest() {
-//    public WsAttributeAssignValue makeWsAttributeAssignValue(String time) {
-//
-//        WsAttributeAssignValue dateTimeValue = new WsAttributeAssignValue();
-//        dateTimeValue.setValueSystem(time);
-//
-//        return dateTimeValue;
+        String time = Dates.formatDate(LocalDateTime.now(), "yyyyMMdd'T'HHmm");
+
+        WsAttributeAssignValue attributeAssignValue = gfs.makeWsAttributeAssignValue(time);
+        assertEquals(time, attributeAssignValue.getValueSystem());
     }
 
     @Test
