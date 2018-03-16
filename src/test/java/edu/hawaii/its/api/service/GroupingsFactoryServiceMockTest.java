5034aead10bb92478a86b75792c87a7bc5e70980
blaine-wataru <bwataru808@yahoo.com>
2018-03-16 11:57:55 -1000

    working on makewsaddmemberresulttest

--- src/test/java/edu/hawaii/its/api/service/GroupingsFactoryServiceMockTest.java
+++ src/test/java/edu/hawaii/its/api/service/GroupingsFactoryServiceMockTest.java
@@ -13,7 +13,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -23,6 +25,30 @@ import static org.junit.Assert.assertTrue;
 @SpringBootTest(classes = {SpringBootWebApplication.class})
 @WebAppConfiguration
 public class GroupingsFactoryServiceMockTest {
+
+    private static final String PATH_ROOT = "path:to:grouping";
+
+    private static final String GROUPING_0_PATH = PATH_ROOT + 0;
+    private static final String GROUPING_1_PATH = PATH_ROOT + 1;
+    private static final String GROUPING_2_PATH = PATH_ROOT + 2;
+    private static final String GROUPING_3_PATH = PATH_ROOT + 3;
+    private static final String GROUPING_4_PATH = PATH_ROOT + 4;
+
+    private static final String GROUPING_0_INCLUDE_PATH = GROUPING_0_PATH + ":include";
+    private static final String GROUPING_0_OWNERS_PATH = GROUPING_0_PATH + ":owners";
+
+    private static final String GROUPING_1_INCLUDE_PATH = GROUPING_1_PATH + ":include";
+    private static final String GROUPING_1_EXCLUDE_PATH = GROUPING_1_PATH + ":exclude";
+
+    private static final String GROUPING_2_INCLUDE_PATH = GROUPING_2_PATH + ":include";
+    private static final String GROUPING_2_EXCLUDE_PATH = GROUPING_2_PATH + ":exclude";
+    private static final String GROUPING_2_BASIS_PATH = GROUPING_2_PATH + ":basis";
+    private static final String GROUPING_2_OWNERS_PATH = GROUPING_2_PATH + ":owners";
+
+    private static final String GROUPING_3_INCLUDE_PATH = GROUPING_3_PATH + ":include";
+    private static final String GROUPING_3_EXCLUDE_PATH = GROUPING_3_PATH + ":exclude";
+
+    private static final String GROUPING_4_EXCLUDE_PATH = GROUPING_4_PATH + ":exclude";
 
     GrouperFactoryServiceImpl gfs = new GrouperFactoryServiceImpl();
 
@@ -84,6 +110,13 @@ public class GroupingsFactoryServiceMockTest {
     //todo
     @Test
     public void makeWsAddMemberResultsTest() {
+        WsAddMemberResults results;
+        List<String> members = new ArrayList<>();
+        members.add("username");
+        WsSubjectLookup lookup = new WsSubjectLookup();
+
+        results = gfs.makeWsAddMemberResults(GROUPING_3_INCLUDE_PATH, lookup, members);
+        assertTrue(results.getResultMetadata().getResultCode().startsWith("SUCCESS"));
     }
 
     //todo
