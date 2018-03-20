05cba7ee268d169cfab28d41ba403403c282f4a9
blaine-wataru <bwataru808@yahoo.com>
2018-03-20 12:44:38 -1000

    WIP fixing up groupingsfactoryservicemocktest

--- src/test/java/edu/hawaii/its/api/service/GroupingsFactoryServiceMockTest.java
+++ src/test/java/edu/hawaii/its/api/service/GroupingsFactoryServiceMockTest.java
@@ -29,32 +29,7 @@ import static org.junit.Assert.assertTrue;
 @WebAppConfiguration
 public class GroupingsFactoryServiceMockTest {
 
-    private static final String PATH_ROOT = "path:to:grouping";
-
-    private static final String GROUPING_0_PATH = PATH_ROOT + 0;
-    private static final String GROUPING_1_PATH = PATH_ROOT + 1;
-    private static final String GROUPING_2_PATH = PATH_ROOT + 2;
-    private static final String GROUPING_3_PATH = PATH_ROOT + 3;
-    private static final String GROUPING_4_PATH = PATH_ROOT + 4;
-
-    private static final String GROUPING_0_INCLUDE_PATH = GROUPING_0_PATH + ":include";
-    private static final String GROUPING_0_OWNERS_PATH = GROUPING_0_PATH + ":owners";
-
-    private static final String GROUPING_1_INCLUDE_PATH = GROUPING_1_PATH + ":include";
-    private static final String GROUPING_1_EXCLUDE_PATH = GROUPING_1_PATH + ":exclude";
-
-    private static final String GROUPING_2_INCLUDE_PATH = GROUPING_2_PATH + ":include";
-    private static final String GROUPING_2_EXCLUDE_PATH = GROUPING_2_PATH + ":exclude";
-    private static final String GROUPING_2_BASIS_PATH = GROUPING_2_PATH + ":basis";
-    private static final String GROUPING_2_OWNERS_PATH = GROUPING_2_PATH + ":owners";
-
-    private static final String GROUPING_3_INCLUDE_PATH = GROUPING_3_PATH + ":include";
-    private static final String GROUPING_3_EXCLUDE_PATH = GROUPING_3_PATH + ":exclude";
-
-    private static final String GROUPING_4_EXCLUDE_PATH = GROUPING_4_PATH + ":exclude";
-
     GrouperFactoryServiceImpl gfs = new GrouperFactoryServiceImpl();
-    GrouperFactoryServiceImplLocal gfsl = new GrouperFactoryServiceImplLocal();
 
     //todo
     @Test
@@ -114,14 +89,6 @@ public class GroupingsFactoryServiceMockTest {
     //todo
     @Test
     public void makeWsAddMemberResultsTest() {
-        WsAddMemberResults results;
-        List<String> members = new ArrayList<>();
-        members.add("username");
-        WsSubjectLookup lookup = new WsSubjectLookup(null, null, "username");
-
-        //todo java.lang.IllegalArgumentException: host parameter is null (Net says "http://" is missing, but how does that apply)
-        results = gfsl.makeWsAddMemberResults(GROUPING_3_PATH, lookup, members);
-        assertTrue(results.getResultMetadata().getResultCode().startsWith("SUCCESS"));
     }
 
     //todo
