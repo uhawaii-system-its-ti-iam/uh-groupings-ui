09f74ccca8056a8cbb35ea93323a658b202eef58
zknoebel <zknoebel@hawaii.edu>
2018-03-23 13:27:33 -1000

    test string for raven

--- src/main/java/edu/hawaii/its/api/type/GroupingsHTTPException.java
+++ src/main/java/edu/hawaii/its/api/type/GroupingsHTTPException.java
@@ -2,6 +2,7 @@ package edu.hawaii.its.api.type;
 
 public class GroupingsHTTPException extends RuntimeException{
     private Integer statusCode = null;
+    private String string = null;
 
     public GroupingsHTTPException() {
         //empty
@@ -18,10 +19,14 @@ public class GroupingsHTTPException extends RuntimeException{
     public GroupingsHTTPException(String message, Throwable cause, int statusCode) {
         super(message, cause);
         this.statusCode = statusCode;
+        this.string = this.toString();
     }
 
     public Integer getStatusCode() {
         return statusCode;
     }
 
+    public String getString() {
+        return string;
+    }
 }
