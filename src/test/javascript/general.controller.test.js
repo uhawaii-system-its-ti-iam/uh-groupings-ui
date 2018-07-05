8031aacb983ef055fde7a125e4cc4789d9466d90
r-robles <rfrobles4@gmail.com>
2018-07-05 12:10:58 -1000

    Create general controller initialization test

---
+++ src/test/javascript/general.controller.test.js
@@ -0,0 +1,44 @@
+describe("GeneralController", function () {
+
+    // Set up mock element for setting the current user
+    var mockElement = document.createElement("div");
+    mockElement.innerHTML = "jdoe";
+    document.getElementById = jasmine.createSpy("name").and.returnValue(mockElement);
+
+    beforeEach(module("UHGroupingsApp"));
+    beforeEach(module("ngMockE2E"));
+
+    var scope;
+    var controller;
+    var httpBackend;
+    var BASE_URL;
+
+    beforeEach(inject(function ($rootScope, $controller, _BASE_URL_, _$httpBackend_) {
+        scope = $rootScope.$new();
+        controller = $controller("GeneralJsController", {
+            $scope: scope
+        });
+        httpBackend = _$httpBackend_;
+        BASE_URL = _BASE_URL_;
+    }));
+
+    it("should define the general controller", function () {
+        expect(controller).toBeDefined();
+    });
+
+    it("should define the variables and methods in the table controller", function () {
+        expect(scope.columnSort).toBeDefined();
+        expect(scope.groupToPages).toBeDefined();
+        expect(scope.filter).toBeDefined();
+        expect(scope.pageRange).toBeDefined();
+        expect(scope.setPage).toBeDefined();
+        expect(scope.disableFirstAndPrev).toBeDefined();
+        expect(scope.disableNextAndLast).toBeDefined();
+        expect(scope.sortBy).toBeDefined();
+    });
+
+    it("should correctly set the currentUser", function () {
+        expect(scope.currentUser).toEqual("jdoe");
+    });
+
+});
