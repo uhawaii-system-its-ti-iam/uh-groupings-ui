fe699bf280887a407e0a4cab0c3c4cdcbe5cacd0
r-robles <rfrobles4@gmail.com>
2018-07-10 13:01:51 -1000

    Add tests for addInBasis and addWhereListed

--- src/test/javascript/general.controller.test.js
+++ src/test/javascript/general.controller.test.js
@@ -41,4 +41,191 @@ describe("GeneralController", function () {
         expect(scope.currentUser).toEqual("jdoe");
     });
 
+    // Set up mock data
+    beforeEach(function () {
+        scope.selectedGrouping = { name: "grouping1", path: "path:path2:grouping1" };
+        scope.itemsPerPage = 20;
+
+        scope.groupingBasis = [
+            {
+                name: "User One",
+                username: "user1",
+                uuid: "00000001",
+                firstName: "User",
+                lastName: "One"
+            },
+            {
+                name: "User Four",
+                username: "user4",
+                uuid: "00000004",
+                firstName: "User",
+                lastName: "Four"
+            },
+            {
+                name: "User Seven",
+                username: "user7",
+                uuid: "00000007",
+                firstName: "User",
+                lastName: "Seven"
+            }
+        ];
+        scope.pagedItemsBasis = scope.groupToPages(scope.groupingBasis);
+
+        scope.groupingInclude = [
+            {
+                name: "User One",
+                username: "user1",
+                uuid: "00000001",
+                firstName: "User",
+                lastName: "One"
+            },
+            {
+                name: "User Two",
+                username: "user2",
+                uuid: "00000002",
+                firstName: "User",
+                lastName: "Two"
+            },
+            {
+                name: "User Three",
+                username: "user3",
+                uuid: "00000003",
+                firstName: "User",
+                lastName: "Three"
+            }
+        ];
+        scope.pagedItemsInclude = scope.groupToPages(scope.groupingInclude);
+
+        scope.groupingExclude = [
+            {
+                name: "User Four",
+                username: "user4",
+                uuid: "00000004",
+                firstName: "User",
+                lastName: "Four"
+            },
+            {
+                name: "User Five",
+                username: "user5",
+                uuid: "00000005",
+                firstName: "User",
+                lastName: "Five"
+            }
+        ];
+        scope.pagedItemsExclude = scope.groupToPages(scope.groupingExclude);
+
+        scope.groupingMembers = [
+            {
+                name: "User One",
+                username: "user1",
+                uuid: "00000001",
+                firstName: "User",
+                lastName: "One"
+            },
+            {
+                name: "User Two",
+                username: "user2",
+                uuid: "00000002",
+                firstName: "User",
+                lastName: "Two"
+            },
+            {
+                name: "User Three",
+                username: "user3",
+                uuid: "00000003",
+                firstName: "User",
+                lastName: "Three"
+            },
+            {
+                name: "User Seven",
+                username: "user7",
+                uuid: "00000007",
+                firstName: "User",
+                lastName: "Seven"
+            }
+        ];
+        scope.pagedItemsMembers = scope.groupToPages(scope.groupingMembers);
+
+        scope.groupingOwners = [
+            {
+                name: "User Six",
+                username: "user6",
+                uuid: "00000006",
+                firstName: "User",
+                lastName: "Six"
+            }
+        ];
+        scope.pagedItemsOwners = scope.groupToPages(scope.groupingOwners);
+
+        scope.listserv = true;
+        scope.allowOptIn = true;
+        scope.allowOptOut = true;
+    });
+
+    // For reference (in index order):
+    // Members: User One, User Two, User Three, User Seven
+    // Basis: User One, User Four, User Seven
+    // Include: User One, User Two, User Three
+    // Exclude: User Four, User Five
+    // Owners: User Six
+    describe("addInBasis", function () {
+        it("should add a key called 'inBasis' for all members in the group passed", function () {
+            scope.addInBasis(scope.groupingInclude);
+            expect(_.has(scope.groupingInclude[0], "inBasis")).toBe(true);
+            expect(_.has(scope.groupingInclude[1], "inBasis")).toBe(true);
+            expect(_.has(scope.groupingInclude[2], "inBasis")).toBe(true);
+
+            scope.addInBasis(scope.groupingExclude);
+            expect(_.has(scope.groupingExclude[0], "inBasis")).toBe(true);
+            expect(_.has(scope.groupingExclude[1], "inBasis")).toBe(true);
+        });
+
+        it("should have a value of 'Yes' for the 'inBasis' key if the member is in the basis group", function () {
+            scope.addInBasis(scope.groupingInclude);
+            expect(scope.groupingInclude[0].inBasis).toEqual("Yes");
+
+            scope.addInBasis(scope.groupingExclude);
+            expect(scope.groupingExclude[0].inBasis).toEqual("Yes");
+        });
+
+        it("should have a value of 'No' for the 'inBasis' key if the member is not in the basis group", function () {
+            scope.addInBasis(scope.groupingInclude);
+            expect(scope.groupingInclude[1].inBasis).toEqual("No");
+            expect(scope.groupingInclude[2].inBasis).toEqual("No");
+
+            scope.addInBasis(scope.groupingExclude);
+            expect(scope.groupingExclude[1].inBasis).toEqual("No");
+        });
+
+    });
+
+    describe("addWhereListed", function () {
+        it("should add a key called 'whereListed' for all members in the grouping", function () {
+            scope.addWhereListed(scope.groupingMembers);
+
+            expect(_.has(scope.groupingMembers[0], "whereListed")).toBe(true);
+            expect(_.has(scope.groupingMembers[1], "whereListed")).toBe(true);
+            expect(_.has(scope.groupingMembers[2], "whereListed")).toBe(true);
+        });
+
+        it("should have a value of 'Basis' for the 'whereListed' key if the member is only in the basis group", function () {
+            scope.addWhereListed(scope.groupingMembers);
+
+            expect(scope.groupingMembers[3].whereListed).toEqual("Basis");
+        });
+
+        it("should have a value of 'Include' for the 'whereListed' key if the member is only in the include group", function () {
+            scope.addWhereListed(scope.groupingMembers);
+
+            expect(scope.groupingMembers[1].whereListed).toEqual("Include");
+            expect(scope.groupingMembers[2].whereListed).toEqual("Include");
+        });
+
+        it("should have a value of 'Basis / Include' for the 'whereListed' key if the member is in both the basis and include group", function () {
+            scope.addWhereListed(scope.groupingMembers);
+
+            expect(scope.groupingMembers[0].whereListed).toEqual("Basis / Include");
+        });
+    });
+
 });
