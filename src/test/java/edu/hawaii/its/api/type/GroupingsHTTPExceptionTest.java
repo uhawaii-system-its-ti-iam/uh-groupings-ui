04ec764981e5f3cd284ee2bb0891cc6483af8a67
Ryan Li <ryanli80@hawaii.edu>
2018-07-17 17:49:54 -1000

    Add GroupingsHTTPException Test

---
+++ src/test/java/edu/hawaii/its/api/type/GroupingsHTTPExceptionTest.java
@@ -0,0 +1,49 @@
+package edu.hawaii.its.api.type;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GroupingsHTTPExceptionTest extends RuntimeException {
+
+    private GroupingsHTTPException groupingsHTTPexception;
+
+    @Before
+    public void setUp() {
+        groupingsHTTPexception = new GroupingsHTTPException();
+    }
+
+
+    @Test
+    public void ConstructionGroupingsHTTPException() {
+        assertNotNull(groupingsHTTPexception);
+        assertNull(groupingsHTTPexception.getExceptionMessage());
+        assertNull(groupingsHTTPexception.getMessage());
+        assertNull(groupingsHTTPexception.getCause());
+
+        Exception e = new RuntimeException("Test Case");
+
+        groupingsHTTPexception = new GroupingsHTTPException("Error", e, 1);
+        assertThat(groupingsHTTPexception.getMessage(), equalTo("Error"));
+        assertThat(groupingsHTTPexception.getCause(), equalTo(e));
+        assertThat(groupingsHTTPexception.getStatusCode(), equalTo(1));
+    }
+
+    @Test
+    public void testGroupings() {
+        groupingsHTTPexception = new GroupingsHTTPException("Error 1");
+        assertThat(groupingsHTTPexception.getMessage(), equalTo("Error 1"));
+    }
+
+    @Test
+    public void tester() {
+        Exception e1 = new RuntimeException("File not found");
+        groupingsHTTPexception = new GroupingsHTTPException("Error 2", e1);
+        assertThat(groupingsHTTPexception.getMessage(), equalTo("Error 2"));
+        assertThat(groupingsHTTPexception.getCause(), equalTo(e1));
+    }
+}
