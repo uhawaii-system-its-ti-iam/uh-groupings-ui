56d4672a70605def4e2defa950cc5297f9ab0496
Michael Hodges <mhodgesatuh@users.noreply.github.com>
2018-07-26 14:15:13 -1000

    Add prod profile to configuration.
    Activate the prod profile for use with the custom.properties file.

--- src/main/java/edu/hawaii/its/groupings/configuration/AppConfigRun.java
+++ src/main/java/edu/hawaii/its/groupings/configuration/AppConfigRun.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
-@Profile(value = { "localhost", "test", "integrationTest", "qa" })
+@Profile(value = { "localhost", "test", "integrationTest", "qa", "prod" })
 @Configuration
 @ComponentScan(basePackages = "edu.hawaii.its")
 @PropertySources({
