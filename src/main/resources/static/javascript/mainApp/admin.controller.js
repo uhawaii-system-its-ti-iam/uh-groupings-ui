ee079d92263be3bc6454be1f0ec93e783f6ddccd
r-robles <rfrobles4@gmail.com>
2018-08-28 13:42:21 -1000

    Fix and update modal designs

--- src/main/resources/static/javascript/mainApp/admin.controller.js
+++ src/main/resources/static/javascript/mainApp/admin.controller.js
@@ -120,8 +120,11 @@
             $scope.userToRemove = options.user;
             $scope.listName = options.listName;
 
+            var windowClass = $scope.showWarningRemovingSelf() ? 'modal-danger' : '';
+
             $scope.removeModalInstance = $uibModal.open({
                 templateUrl: "modal/removeModal.html",
+                windowClass: windowClass,
                 scope: $scope
             });
 
