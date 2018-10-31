9e8aae3e1569a70bfd606e5f4d4c05d2f7610f4a
zknoebel <zknoebel@hawaii.edu>
2018-10-31 11:24:40 -1000

    WIP recieve error codes from HTTP responses recieved from the API

--- src/main/java/edu/hawaii/its/api/service/HttpRequestServiceImpl.java
+++ src/main/java/edu/hawaii/its/api/service/HttpRequestServiceImpl.java
@@ -1,7 +1,10 @@
 package edu.hawaii.its.api.service;
 
+import edu.hawaii.its.api.controller.RestTemplateResponseErrorHandler;
 import edu.hawaii.its.api.type.GroupingsHTTPException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -15,21 +18,24 @@ public class HttpRequestServiceImpl implements HttpRequestService {
     @Value("${groupings.api.current_user}")
     private String CURRENT_USER;
 
+    private RestTemplate restTemplate;
+
+    @Autowired
+    public HttpRequestServiceImpl(RestTemplateBuilder restTemplateBuilder) {
+        //todo should this not be declaring a new variable named restTemplate?
+        restTemplate = restTemplateBuilder
+                .errorHandler(new RestTemplateResponseErrorHandler())
+                .build();
+    }
+
     @Override
     public ResponseEntity makeApiRequest(String currentUser, String uri, HttpMethod method, Class responseClass) {
 
-        RestTemplate restTemplate = new RestTemplate();
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.set(CURRENT_USER, currentUser);
         HttpEntity httpEntity = new HttpEntity(httpHeaders);
 
-        try {
-            return restTemplate.exchange(uri, method, httpEntity, responseClass);
-        } catch (Exception e) {
-            GroupingsHTTPException ge = new GroupingsHTTPException("API Error", e);
-            return ResponseEntity
-                    .badRequest()
-                    .body(ge);
-        }
+        //todo why do we need the class fromm the body of the rest template rather than RestTemplate.class?
+        return restTemplate.exchange(uri, method, httpEntity, responseClass);
     }
 }
