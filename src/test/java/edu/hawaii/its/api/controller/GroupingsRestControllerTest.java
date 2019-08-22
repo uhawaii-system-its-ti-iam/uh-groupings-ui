e1affd3606bd945f08f74a96947a3416fc5b00c1
Irene <33890391+Irene-ma@users.noreply.github.com>
2019-08-22 09:52:55 -1000

    delete unused import calls

--- src/test/java/edu/hawaii/its/api/controller/GroupingsRestControllerTest.java
+++ src/test/java/edu/hawaii/its/api/controller/GroupingsRestControllerTest.java
@@ -25,7 +25,6 @@ import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
