ecb2226d1ddbb8d9221d2138bb8fd893c39aa020
janeligio <jeligio808@gmail.com>
2019-09-12 15:08:47 -1000

    Revert "Merge branch 'master' into dev-jan"
    This reverts commit be89b2015a5f770d3226d1f68ea08a96ed22ef4e, reversing
    changes made to a76ae6c326776ce4aa61ce12b562c1944072b94c.

--- src/main/resources/static/javascript/mainApp/general.controller.js
+++ src/main/resources/static/javascript/mainApp/general.controller.js
@@ -413,18 +413,15 @@
          * @param {string} list - the list the user is being added to (either Include or Exclude)
          */
         $scope.addMember = function (list) {
-            var groupingPath = $scope.selectedGrouping.path;
+            const groupingPath = $scope.selectedGrouping.path;
             groupingsService.getGrouping(groupingPath, 1, PAGE_SIZE, "name", true, function () {
-                var userToAdd = $scope.userToAdd;
-                var inBasis = _.some($scope.groupingBasis, {username: userToAdd});
+                const userToAdd = $scope.userToAdd;
                 if (_.isEmpty(userToAdd)) {
                     $scope.createAddErrorModal(userToAdd);
                 } else if ($scope.existInList(userToAdd, list)) {
-                    $scope.createCheckModal(userToAdd, list, false, inBasis);
+                    $scope.createCheckModal(userToAdd, list, false);
                 } else if ($scope.isInAnotherList(userToAdd, list)) {
-                    $scope.createCheckModal(userToAdd, list, true, inBasis);
-                } else if ((inBasis && list == "Include") || (!inBasis && list == "Exclude" )) {
-                    $scope.createBasisWarningModal(userToAdd, list, inBasis);
+                    $scope.createCheckModal(userToAdd, list, true);
                 } else {
                     $scope.createConfirmAddModal({
                         userToAdd: userToAdd,
@@ -473,9 +470,6 @@
             }
         };
 
-        $scope.changeStyleAttribute = function (id, attr, setAs) {
-            document.getElementById(id).style[attr] = setAs;
-        };
         /**
          * Initiates the adding of a member to a list.
          * @param {string} userToAdd - user being added
@@ -543,11 +537,10 @@
          * @param user - Username of the user they are trying to add.
          * @param listName - name of the list they are adding to (either Include or Exclude)
          */
-        $scope.createCheckModal = function (user, listName, swap, inBasis) {
+        $scope.createCheckModal = function (user, listName, swap) {
             $scope.user = user;
             $scope.listName = listName;
             $scope.swap = swap;
-            $scope.inBasis = inBasis;
 
             $scope.checkModalInstance = $uibModal.open({
                 templateUrl: "modal/checkModal",
@@ -1148,36 +1141,6 @@
                 //do nothing
             });
         };
-
-        /**
-         * Creates warning modal if user is in being added to include and is a basis member or if
-         * a user is being added to exclude and is not a basis member
-         * @param user - user being added
-         * @param listName - grouping list (i.e. include or exclude)
-         * @param inBasis - boolean if user is in basis or not
-         */
-        $scope.createBasisWarningModal = function (user, listName, inBasis) {
-            $scope.user = user;
-            $scope.listName = listName;
-            $scope.inBasis = inBasis;
-
-            $scope.basisWarningModalInstance = $uibModal.open({
-                templateUrl: "modal/basisWarningModal",
-                scope: $scope
-            });
-
-            $scope.basisWarningModalInstance.result.then(function () {
-                $scope.updateAddMember(user, listName)
-            });
-        };
-
-        $scope.proceedBasisWarningModal = function () {
-            $scope.basisWarningModalInstance.close();
-        };
-
-        $scope.closeBasisWarningModal = function () {
-            $scope.basisWarningModalInstance.dismiss();
-        }
 
         /**
          * Proceeds with the syncDest confirmation
