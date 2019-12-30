1f643d909f303a0c261a211418ae1138615b7b56
jasonhuang23 <jasonhuang2343@gmail.com>
2019-12-30 12:12:55 -1000

    GROUPINGS-400 Added comments and made sure calculations were correct for difference between dates.

--- src/main/resources/static/javascript/mainApp/outage.controller.js
+++ src/main/resources/static/javascript/mainApp/outage.controller.js
@@ -1,26 +1,36 @@
 (function () {
-
+    /**
+     * Controller for the planned maintenance modal. When the current date is two or one day away from the maintenance date, the modal will alert the user about the date and time which UH groupings will be down, and when it will be back up again.
+     * @param $scope - Binding between controller and HTML page
+     * @param $uibModal - Creates modal
+     * @constructor
+     */
     function OutageJsController($scope, $uibModal) {
 
         /**
-         * Planned Maintenance Warning Modal
+         * Instatiating the planned maintenance, current, and
+         * back online dates.
          */
-
-
-        $scope.planned = new Date('2020-01-02'); // Set the date which UH groupings will be down.
+        $scope.planned = new Date('2020-01-01'); // Set the date which UH groupings will be down.
         $scope.today = new Date(); // Current date.
         $scope.backOnline = new Date('2020-01-02'); // Set the date which UH groupings will be back online.
 
-        var Difference_In_Time = $scope.planned.getTime() - $scope.today.getTime(); // Calculate the time apart from planned date to current
-        var Difference_In_Day = Difference_In_Time / (1000 * 3600 * 24); //
-        var numDays = Math.round(Difference_In_Day);
-
         /**
-         * Setting time of planned maintenance.
+         * Setting the time UH groupings will be offline.
          */
+
         $scope.planned.setHours(1 + 24); // Set the hour which UH groupings will be down.
         // Using a 24 hour format.
         $scope.planned.setMinutes(25) // Set the minute which UH groupings will be down.
+
+        /**
+         * Calculating the days apart from planned and current date.
+         */
+
+        var Difference_In_Time = $scope.planned.getTime() - $scope.today.getTime(); // Calculate the time apart from planned date to current date.
+        var Difference_In_Day = Difference_In_Time / (1000 * 3600 * 24);  // Calculate days apart from planned date to current date.
+        var numDays = Math.round(Difference_In_Day);
+
 
         $scope.openPlannedOutageModal = function() {
             $scope.outageModalInstance = $uibModal.open({
@@ -35,7 +45,11 @@
             }
         };
 
-        if(numDays == 5) {
+        /**
+         * Modal should open if the current date is either
+         * 2 days away or 1 day away.
+         */
+        if(numDays == 2 || numDays == 1) {
             $scope.openPlannedOutageModal();
         }
 
@@ -48,9 +62,36 @@
         $scope.backOnline.setMinutes(10);
 
 
+        /**
+         * $scope.myvalue = false;
+         * $scope.showAlert = function() {
+         *     var numDays = 12;
+         *     if(numDays != 12) {
+         *         $scope.myvalue = true;
+         *     }
+         *     else {
+         *         $scope.myvalue = false;
+         *     }
+         * }
+         */
+
+
+        /** menubar.html
+         * <div ng-controller="OutageJsController">
+         *     <div ng-show="myvalue" class="ng-cloak">
+         *         [What is being shown]
+         *         </div>
+         * <div id="containerDetailsTicket" class="clearfix" ng-show="showAlert()">
+         *     </div>
+         *
+         *
+         * <div>
+         */
 
 
     }
     UHGroupingsApp.controller("OutageJsController", OutageJsController);
 })();
+
+
 
