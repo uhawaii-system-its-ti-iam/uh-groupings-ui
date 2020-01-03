54b77659bf4bd08c3c2b58da9752464d0c1e510f
jasonhuang23 <jasonhuang2343@gmail.com>
2020-01-03 12:59:41 -1000

    GROUPINGS-400 Added showAlert() function that displays notification bar for planned outages.

--- src/main/resources/static/javascript/mainApp/outage.controller.js
+++ src/main/resources/static/javascript/mainApp/outage.controller.js
@@ -11,7 +11,7 @@
          * Instatiating the planned maintenance, current, and
          * back online dates.
          */
-        $scope.planned = new Date('2020-01-04'); // Set the date which UH groupings will be down.
+        $scope.planned = new Date('2020-01-01'); // Set the date which UH groupings will be down.
         $scope.today = new Date(); // Current date.
         $scope.backOnline = new Date('2020-01-02'); // Set the date which UH groupings will be back online.
 
@@ -52,7 +52,9 @@
         $scope.myvalue = false;
         if(numDays == 2 || numDays == 1) {
             $scope.openPlannedOutageModal();
-            $scope.myvalue = true;
+            $scope.showAlert = function() {
+                $scope.myvalue = true;
+            }
         }
         else {
             $scope.myvalue = false;
