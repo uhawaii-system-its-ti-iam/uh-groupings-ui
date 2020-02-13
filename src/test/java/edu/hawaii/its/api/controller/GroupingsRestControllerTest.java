611ffa11f4fd36d6826d7933a2d1b828509d5c29
Nadine Alcantara <nadine-alcantara@users.noreply.github.com>
2020-02-13 10:02:04 -1000

    Fixed Mokito Matchers class deprication issue

--- src/test/java/edu/hawaii/its/api/controller/GroupingsRestControllerTest.java
+++ src/test/java/edu/hawaii/its/api/controller/GroupingsRestControllerTest.java
@@ -19,7 +19,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
