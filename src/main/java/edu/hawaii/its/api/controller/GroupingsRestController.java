7d623c3504c92f7f93bece4f8314ace6741d43fe
zakgilbert <gilbertz@hawaii.edu>
2020-02-27 12:09:15 -1000

    Use explicit scoping instead of the default package private level

--- src/main/java/edu/hawaii/its/api/controller/GroupingsRestController.java
+++ src/main/java/edu/hawaii/its/api/controller/GroupingsRestController.java
@@ -312,8 +312,8 @@ public class GroupingsRestController {
         logger.info("Entered REST assignOwnership...");
 
         String safeGrouping = policy.sanitize(grouping);
-
         String uri = String.format(API_2_1_BASE + "/groupings/%s/owners/%s", safeGrouping, newOwner);
+
         return httpRequestService.makeApiRequest(principal.getName(), uri, HttpMethod.PUT);
     }
 
