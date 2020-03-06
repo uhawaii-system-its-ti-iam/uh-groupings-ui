1f0572465a4493b10bf12016a7ff810edd64c891
zakgilbert <gilbertz@hawaii.edu>
2020-03-05 14:19:21 -1000

    Implement Restful mappings for delete multiple use functionality

--- src/main/resources/static/javascript/mainApp/groupings.service.js
+++ src/main/resources/static/javascript/mainApp/groupings.service.js
@@ -164,6 +164,10 @@
                 let endpoint = BASE_URL + path + "/" + member + "/deleteMemberFromIncludeGroup";
                 dataProvider.updateData(onSuccess, onError, endpoint);
             },
+            removeMembersFromInclude(path, members, onSuccess, onError) {
+                let endpoint = BASE_URL + path + "/" + members + "/deleteMembersFromIncludeGroup";
+                dataProvider.updateData(onSuccess, onError, endpoint);
+            },
 
             /**
              * Removes a member from the exclude group of a grouping.
@@ -174,6 +178,11 @@
              */
             removeMemberFromExclude(path, member, onSuccess, onError) {
                 let endpoint = BASE_URL + path + "/" + member + "/deleteMemberFromExcludeGroup";
+                dataProvider.updateData(onSuccess, onError, endpoint);
+            },
+
+            removeMembersFromExclude(path, members, onSuccess, onError) {
+                let endpoint = BASE_URL + path + "/" + members + "/deleteMembersFromExcludeGroup";
                 dataProvider.updateData(onSuccess, onError, endpoint);
             },
 
