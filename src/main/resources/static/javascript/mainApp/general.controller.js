757a3f8aef40593c4f371244ed6d593721442cef
Ryota Seki <ryotabs@hawaii.edu>
2020-04-23 10:41:11 -1000

    Remove more commented out code

--- src/main/resources/static/javascript/mainApp/general.controller.js
+++ src/main/resources/static/javascript/mainApp/general.controller.js
@@ -635,17 +635,6 @@
             let fun = "addMembersTo";
             await groupingsService[(listName === "Include") ? (fun + "Include") : (fun + "Exclude")]
             (groupingPath, list, handleSuccessfulAdd, handleUnsuccessfulRequest, timeoutModal);
-
-
-            /*
-             if (listName === "Include")
-                 await groupingsService.addMembersToInclude(groupingPath, list, handleSuccessfulAdd,
-                     handleUnsuccessfulRequest, timeoutModal);
-             else if (listName === "Exclude")
-                 await groupingsService.addMembersToExclude(groupingPath, list, handleSuccessfulAdd,
-                     handleUnsuccessfulRequest, timeoutModal);
-
-             */
         };
 
         /**
