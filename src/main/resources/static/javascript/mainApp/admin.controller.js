6c19b26a4610916f0103e0e70999e190cc97c7ca
zakgilbert <gilbertz@hawaii.edu>
2021-02-08 11:32:46 -1000

    Remove extra calls to adminsLists which were nested in add/remove.

--- src/main/resources/static/javascript/mainApp/admin.controller.js
+++ src/main/resources/static/javascript/mainApp/admin.controller.js
@@ -208,32 +208,25 @@
         };
 
         /**
-         * Adds a user to the admin list.
+         * Add a user to the admin list.
          */
         $scope.addAdmin = function () {
             $scope.waitingForImportResponse = true;
-            groupingsService.getAdminLists(function () {
-                const adminToAdd = $scope.adminToAdd;
-
-                if (_.isEmpty(adminToAdd)) {
-                    $scope.emptyInput = true;
+            const adminToAdd = $scope.adminToAdd;
+            if (_.isEmpty(adminToAdd)) {
+                $scope.emptyInput = true;
+            } else {
+                if ($scope.inAdminList(adminToAdd)) {
+                    $scope.user = adminToAdd;
+                    $scope.listName = "admins";
+                    $scope.swap = false;
                 } else {
-                    if ($scope.inAdminList(adminToAdd)) {
-                        $scope.user = adminToAdd;
-                        $scope.listName = "admins";
-                        $scope.swap = false;
-                    } else {
-                        $scope.createConfirmAddModal({
-                            userToAdd: adminToAdd,
-                            listName: "admins"
-                        });
-                    }
+                    $scope.createConfirmAddModal({
+                        userToAdd: adminToAdd,
+                        listName: "admins"
+                    });
                 }
-            }, function (res) {
-                if (res.statusCode === 403) {
-                    $scope.createRoleErrorModal();
-                }
-            });
+            }
             $scope.waitingForImportResponse = false;
         };
 
@@ -244,23 +237,16 @@
          * account
          */
         $scope.removeAdmin = function (currentPage, index) {
-            groupingsService.getAdminLists(function () {
-                const adminToRemove = $scope.pagedItemsAdmins[currentPage][index];
-
-                if ($scope.adminsList.length > 1) {
-                    $scope.createRemoveModal({
-                        user: adminToRemove,
-                        listName: "admins"
-                    });
-                } else {
-                    const userType = "admin";
-                    $scope.createRemoveErrorModal(userType);
-                }
-            }, function (res) {
-                if (res.statusCode === 403) {
-                    $scope.createRoleErrorModal();
-                }
-            });
+            const adminToRemove = $scope.pagedItemsAdmins[currentPage][index];
+            if ($scope.adminsList.length > 1) {
+                $scope.createRemoveModal({
+                    user: adminToRemove,
+                    listName: "admins"
+                });
+            } else {
+                const userType = "admin";
+                $scope.createRemoveErrorModal(userType);
+            }
         };
 
         /**
