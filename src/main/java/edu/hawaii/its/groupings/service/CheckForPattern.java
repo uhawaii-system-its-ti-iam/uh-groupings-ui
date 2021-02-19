1593c0a9ab169e231f15f42c636e3f074e81714d
johnbigornia <jbigornia15@gmail.com>
2021-02-19 13:50:42 -1000

    CheckForPattern

---
+++ src/main/java/edu/hawaii/its/groupings/service/CheckForPattern.java
@@ -0,0 +1,63 @@
+
+package edu.hawaii.its.groupings.service;
+
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Service
+public class CheckForPattern {
+
+    @PostConstruct
+    private void Check() throws IOException {
+        boolean detected = false;
+        File dir = new File("src/main/resources");
+        File[] fileResources = dir.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.endsWith(".properties");
+            }
+        });
+
+        ArrayList<Integer> lineNumbers = new ArrayList<>();
+
+        Pattern pattern = Pattern.compile("grouperClient.webService.password");
+        Matcher matcher;
+
+        for (File fr : fileResources) {
+            int lineId = 0;
+            Scanner fileScanner = new Scanner(fr);
+            while(fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                lineId++;
+
+                matcher = pattern.matcher(line);
+
+                if (matcher.find()) {
+                    lineNumbers.add(lineId);
+                }
+            }
+
+            if(!lineNumbers.isEmpty()) {
+                detected = true;
+
+                System.out.print("\n------------------------------------------------------\n\n");
+                System.out.println("Pattern detected in file: " + fr + " in lines:");
+
+                for (int li : lineNumbers) {
+                    System.out.println("Line" + li);
+                }
+
+                System.out.print("\n------------------------------------------------------\n\n");
+            }
+            lineNumbers.removeAll(lineNumbers);
+        }
+    }
+}
