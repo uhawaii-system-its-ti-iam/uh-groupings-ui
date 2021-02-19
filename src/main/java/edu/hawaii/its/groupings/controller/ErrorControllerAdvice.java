c9facbc057a3f9a8520ee636e000d5d0af6c52b6
nzenger <nzenger@hawaii.edu>
2021-02-19 12:03:21 -1000

    Fixed faulty email service beans, and added snedWithStack() function in Email Service to send stack dumps upon errors thrown.

--- src/main/java/edu/hawaii/its/groupings/controller/ErrorControllerAdvice.java
+++ src/main/java/edu/hawaii/its/groupings/controller/ErrorControllerAdvice.java
@@ -4,6 +4,9 @@ import edu.hawaii.its.api.type.GroupingsHTTPException;
 import edu.hawaii.its.api.type.GroupingsServiceResultException;
 import edu.hawaii.its.groupings.access.User;
 import edu.hawaii.its.groupings.access.UserContextService;
+import edu.hawaii.its.groupings.service.EmailService;
+import edu.hawaii.its.groupings.type.Feedback;
+
 import edu.internet2.middleware.grouperClient.ws.GcWebServiceError;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -25,39 +28,56 @@ public class ErrorControllerAdvice {
     @Autowired
     private UserContextService userContextService;
 
+    @Autowired
+    private EmailService emailService;
+
     @ExceptionHandler(GroupingsServiceResultException.class)
     public ResponseEntity<GroupingsHTTPException> handleGroupingsServiceResultException(GroupingsServiceResultException gsre) {
+        Feedback errorFeedback = new Feedback();
+        emailService.sendWithStack(errorFeedback, gsre, "GroupingsServiceResultException");
       return exceptionResponse("Groupings Service resulted in FAILURE", gsre, 400);
     }
 
     @ExceptionHandler (GcWebServiceError.class)
     public ResponseEntity<GroupingsHTTPException> handleGcWebServiceError(GcWebServiceError gce) {
+        Feedback errorFeedback = new Feedback();
+        emailService.sendWithStack(errorFeedback, gce, "GcWebServiceError");
         return exceptionResponse(gce.getMessage(), gce, 404);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<GroupingsHTTPException> handleIllegalArgumentException(IllegalArgumentException iae, WebRequest request) {
+        Feedback errorFeedback = new Feedback();
+        emailService.sendWithStack(errorFeedback, iae, "IllegalArgumentException");
         return exceptionResponse("Resource not available", iae, 404);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<GroupingsHTTPException> handleException(Exception exception) {
+        Feedback errorFeedback = new Feedback();
+        emailService.sendWithStack(errorFeedback, exception, "Exception");
         return exceptionResponse("Exception", exception, 500);
     }
 
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<GroupingsHTTPException> handleRuntimeException(Exception exception) {
+        Feedback errorFeedback = new Feedback();
+        emailService.sendWithStack(errorFeedback, exception, "RuntimeException");
       return exceptionResponse("Runtime Exception", exception, 500);
     }
 
     @ExceptionHandler({MessagingException.class, IOException.class})
     public ResponseEntity<GroupingsHTTPException> handleMessagingException(Exception e) {
+        Feedback errorFeedback = new Feedback();
+        emailService.sendWithStack(errorFeedback, e, "MessagingException");
       return exceptionResponse("Mail service exception", e, 500);
     }
 
     @ExceptionHandler(UnsupportedOperationException.class)
     public ResponseEntity<GroupingsHTTPException> handleUnsupportedOperationException(UnsupportedOperationException nie) {
+        Feedback errorFeedback = new Feedback();
+        emailService.sendWithStack(errorFeedback, nie, "UnsupportedOperationException");
       return exceptionResponse("Method not implemented", nie, 501);
     }
 
@@ -70,7 +90,8 @@ public class ErrorControllerAdvice {
             username = user.getUsername();
         }
         logger.error("username: " + username + "; Exception: ", ex);
-
+        Feedback errorFeedback = new Feedback();
+        emailService.sendWithStack(errorFeedback, ex, "TypeMismatchException");
         return "redirect:/error";
     }
 
@@ -84,7 +105,7 @@ public class ErrorControllerAdvice {
         GroupingsHTTPException httpException = new GroupingsHTTPException(message, cause, status);
 
         logger.error("username: " + username + "; Exception: ", httpException.getCause());
-
+        Feedback errorFeedback = new Feedback();
         return ResponseEntity.status(status).body(httpException);
     }
 }
