b2cdf5b25cab85114d2fd8c21aba4ef92203f02a
Nadine Alcantara <nadine-alcantara@users.noreply.github.com>
2021-02-26 15:48:08 -1000

    Hopefully will fix Codacy issue

--- src/main/resources/static/javascript/mainApp/admin.controller.js
+++ src/main/resources/static/javascript/mainApp/admin.controller.js
@@ -58,6 +58,7 @@
          * Fetch a list of memberships pertaining to $scope.personToLookUp.
          */
         $scope.searchForUserGroupingInformation = function () {
+
             if (_.isEmpty($scope.personToLookup)) {
                 $scope.emptyInput = true;
             } else {
