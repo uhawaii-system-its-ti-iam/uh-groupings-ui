3455e742a105a24e7c1587c81d7b7265c4786565
johnbigornia <jbigornia15@gmail.com>
2021-03-22 11:05:21 -1000

    Started a clean branch and imported failure analyzer code.

---
+++ src/main/java/edu/hawaii/its/groupings/service/PasswordScanner.java
@@ -0,0 +1,39 @@
+package edu.hawaii.its.groupings.service;
+
+import edu.hawaii.its.groupings.exceptions.PasswordFoundException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import edu.hawaii.its.groupings.controller.ErrorRestController;
+
+import org.springframework.stereotype.Service;
+import javax.annotation.PostConstruct;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class PasswordScanner {
+
+    private static final Log logger = LogFactory.getLog(ErrorRestController.class);
+
+    @PostConstruct
+    public void init() throws IOException, PasswordFoundException {
+
+        CheckForPattern checkForPattern = new CheckForPattern();
+
+        List<String> result = checkForPattern.fileLocations(".properties", "src/main/resources", "^.*password.*\\=(?!\\s*$).+");
+
+        String patternResult = "";
+
+        logger.info("PasswordScanner init");
+
+        if (!result.isEmpty()) {
+
+            for (String list: result) {
+                patternResult += "\n" + list;
+            }
+
+            throw new PasswordFoundException(patternResult);
+        }
+    }
+}
