08a044f8b13a4f4404c63750f6a450258570f383
Irene Ma <mairene@hawaii.edu>
2021-04-22 12:09:00 -1000

    register controllers in App

--- src/main/resources/static/javascript/mainApp/app.js
+++ src/main/resources/static/javascript/mainApp/app.js
@@ -1,3 +1,17 @@
 /*  Main app that initializes the angular controller. */
 "use strict";
 const UHGroupingsApp = angular.module("UHGroupingsApp", ["ui.bootstrap"]);
+
+UHGroupingsApp.controller('UHGroupingsApp',function($scope){
+    $scope.message="msg";
+
+});
+UHGroupingsApp.controller('AdminJsController',function($scope){
+    $scope.message="msg";
+
+});
+
+UHGroupingsApp.controller('MembershipJsController',function($scope){
+    $scope.message="msg";
+
+});
