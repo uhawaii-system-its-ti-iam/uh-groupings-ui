6a1d0fd279b8cbf5c1567ee5834db740f7d1d2eb
mikel <mikel123@hawaii.rr.com>
2021-05-26 18:17:34 -1000

    Still need to fix assert

--- src/test/java/edu/hawaii/its/groupings/service/PasswordScannerTest.java
+++ src/test/java/edu/hawaii/its/groupings/service/PasswordScannerTest.java
@@ -1,8 +1,10 @@
 package edu.hawaii.its.groupings.service;
 import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+import java.io.StringWriter;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -10,21 +12,23 @@ import org.junit.Before;
 import org.junit.Test;
 import edu.hawaii.its.groupings.exceptions.PasswordFoundException;
 
+import org.springframework.security.core.parameters.P;
+
 public class PasswordScannerTest {
     File dirname = new File("src/main/resources");
     @Before
     public void setUp() {
         assertTrue("Resource directory does not exist.", dirname.exists());
     }
+
     private File createFile(File path, String pwdValue) throws Exception {
         File file = File.createTempFile("temp", ".properties", path);
-        FileWriter fw1 = new FileWriter(file);
-        BufferedWriter bw1 = new BufferedWriter(fw1);
-        bw1.write("password=" + pwdValue);
-        bw1.close();
-        fw1.close();
+        try (FileWriter w = new FileWriter(file)) {
+            w.write("password=" + pwdValue);
+        }
         return file;
     }
+
     @Test
     public void testCheckForPassWordsFound() throws Exception {
         PasswordScanner passwordScanner = new PasswordScanner();
@@ -32,12 +36,19 @@ public class PasswordScannerTest {
         assertTrue(file.exists());
         try {
             passwordScanner.init();
+            //passwordScanner should have found password in temp file
             fail("Should not reach here.");
         } catch (Exception e) {
             assertThat(e, instanceOf(PasswordFoundException.class));
+            StringWriter sw = new StringWriter();
+            String exceptionAsString = e.toString();
+//            System.out.print(exceptionAsString);
+//            System.out.print("DONE");
+            assertEquals("edu.hawaii.its.groupings.exceptions.PasswordFoundException:\n" + file + "on line: 1", e.getMessage());
         }
         file.delete();
     }
+
     @Test
     public void testCheckForPassWordsNotFound() throws Exception {
         PasswordScanner passwordScanner = new PasswordScanner();
@@ -49,5 +60,24 @@ public class PasswordScannerTest {
             fail("Error: " + e.getMessage());
         }
         file.delete();
+    }
+
+    @Test
+    public void testCheckForPassWordsMultipleFilesFound() throws Exception {
+        PasswordScanner passwordScanner = new PasswordScanner();
+        File file1 = createFile(dirname, "pwd1");
+        File file2 = createFile(dirname, "pwd2");
+        assertTrue(file1.exists());
+        assertTrue(file2.exists());
+        try {
+            passwordScanner.init();
+            //passwordScanner should have found password in temp file
+            fail("Should not reach here.");
+        } catch (Exception e) {
+            assertThat(e, instanceOf(PasswordFoundException.class));
+            e.getMessage();
+        }
+        file1.delete();
+        file2.delete();
     }
 }
