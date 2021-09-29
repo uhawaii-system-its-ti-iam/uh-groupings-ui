fabeeb255a805423c1740dfc32e32589e5052add
zakgilbert <gilbertz@hawaii.edu>
2021-09-29 11:55:32 -1000

    Remove unused vars.

--- src/test/javascript/admin.controller.test.js
+++ src/test/javascript/admin.controller.test.js
@@ -1,23 +1,16 @@
 describe("AdminController", function () {
 
-    // Set up mock element for setting the current user
     beforeEach(module("UHGroupingsApp"));
     beforeEach(module("ngMockE2E"));
 
     let scope;
     let controller;
-    let httpBackend;
-    let BASE_URL;
-    let window;
 
     beforeEach(inject(function ($rootScope, $controller, _BASE_URL_, _$httpBackend_) {
         scope = $rootScope.$new();
         controller = $controller("AdminJsController", {
             $scope: scope,
-            $window: window
         });
-        httpBackend = _$httpBackend_;
-        BASE_URL = _BASE_URL_;
     }));
 
     it("should define the admin controller", function () {
