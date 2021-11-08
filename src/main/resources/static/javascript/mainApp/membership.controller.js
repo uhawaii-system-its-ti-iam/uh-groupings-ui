f2233b0f0337c04c7b85287b58af9fec8ad51d61
ruizaj <60155925+ruizaj@users.noreply.github.com>
2021-11-08 07:57:30 -1000

    Making the membership list to include owned groupings too

--- src/main/resources/static/javascript/mainApp/membership.controller.js
+++ src/main/resources/static/javascript/mainApp/membership.controller.js
@@ -33,7 +33,7 @@
             // Request a list of membership objects from the API.
             groupingsService.getMembershipResults((res) => {
                     // Codacy throws an error regarding the '_' in the uniqBy function. This error will be ignored until a solution is found.
-                  $scope.membershipsList = _.filter(_.sortBy(_.uniqBy(res, "name"), "name"), (membership) => {return membership.inInclude});
+                  $scope.membershipsList = _.filter(_.sortBy(_.uniqBy(res, "name"), "name"), (membership) => {return membership.inInclude || membership.inOwner});
                   $scope.pagedItemsMemberships = $scope.objToPageArray($scope.membershipsList, 20);
                     $scope.loading = false;
                 },
