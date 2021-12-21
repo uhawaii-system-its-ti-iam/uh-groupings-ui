f0e72f75c660b4baf69e7eeab3e5b7da6a1a7ceb
Gavin Peng <gavin4@hawaii.edu>
2021-12-21 13:12:07 -1000

    Fix remove from groups

--- src/main/resources/static/javascript/mainApp/general.controller.js
+++ src/main/resources/static/javascript/mainApp/general.controller.js
@@ -1419,10 +1419,6 @@
             $scope.syncDestArray = [];
         }
 
-        function handleMultiMemberRemove() {
-            $scope.searchForUserGroupingInformation();
-        }
-
         function handleGroupingReset() {
             $scope.getGroupingInformation();
             $scope.loading = false;
@@ -1525,38 +1521,6 @@
             r.open("POST", "/uhgroupings/logout", true);
             r.setRequestHeader("X-XSRF-TOKEN", $scope.getCookie("XSRF-TOKEN"));
             r.send();
-        };
-
-        /**
-         * Create a modal that prompts the user whether they want to delete the user or not. If 'Yes' is pressed, then
-         * a request is made to delete the user.
-         * @param {object} options - the options object
-         * @param {object} options.user - the user being removed
-         * @param {string} options.groupPaths - groups the user is being removed from
-         * @param {string} options.listName - groups the user is being removed from
-         */
-        $scope.createRemoveFromGroupsModal = function (options) {
-            $scope.userToRemove = options.user;
-            $scope.groupPaths = options.groupPaths.join(", ");
-            $scope.listName = options.listName.join(", ");
-
-            const windowClass = $scope.showWarningRemovingSelf() ? "modal-danger" : "";
-
-            $scope.removeModalInstance = $uibModal.open({
-                templateUrl: "modal/removeModal",
-                windowClass: windowClass,
-                scope: $scope,
-                backdrop: "static",
-                keyboard: false
-            });
-
-            $scope.removeModalInstance.result.then(function () {
-                $scope.loading = true;
-                let userToRemove = options.user.username;
-                let groupingPath = $scope.groupPaths;
-                groupingsService.removeFromGroups(groupingPath, userToRemove, handleMultiMemberRemove, handleUnsuccessfulRequest);
-                $scope.personToLookup = userToRemove;
-            });
         };
 
         /**
