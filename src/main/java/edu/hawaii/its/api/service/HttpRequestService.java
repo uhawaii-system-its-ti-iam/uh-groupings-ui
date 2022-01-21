ca6e6b83cd2edfd6bbf634d76808036e4f14b4f2
Cody McCodePants <cody@mccodepants.com>
2022-01-21 13:02:19 -1000

    overloaded methods in HttpRequestService

--- src/main/java/edu/hawaii/its/api/service/HttpRequestService.java
+++ src/main/java/edu/hawaii/its/api/service/HttpRequestService.java
@@ -2,6 +2,9 @@ package edu.hawaii.its.api.service;
 
 import edu.hawaii.its.api.controller.RestTemplateResponseErrorHandler;
 
+import edu.hawaii.its.groupings.access.User;
+import edu.hawaii.its.groupings.access.UserContextService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
@@ -12,41 +15,58 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service("httpRequestService")
-public class HttpRequestService implements HttpRequestService {
+public class HttpRequestService{
+
+	@Autowired
+	private UserContextService userContextService;
 
     @Value("${groupings.api.current_user}")
-    private String CURRENT_USER;
+    private String KEY_CURRENT_USER;
 
     /*
      * Make an http request to the API with path variables.
      */
     @SuppressWarnings("lgtm[java/xss]")
-    @Override
-    public ResponseEntity<String> makeApiRequest(String currentUser, String uri, HttpMethod method) {
+    public ResponseEntity<String> makeApiRequest(String user, String uri, HttpMethod method) {
 
         HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.set(CURRENT_USER, currentUser);
+        httpHeaders.set(KEY_CURRENT_USER, user);
         HttpEntity<String> httpEntity = new HttpEntity<>(httpHeaders);
 
-        RestTemplate restTemplate =
-                new RestTemplateBuilder().errorHandler(new RestTemplateResponseErrorHandler()).build();
+		RestTemplate restTemplate = restTemplate();
         return restTemplate.exchange(uri, method, httpEntity, String.class);
     }
+
+	public ResponseEntity<String> makeApiRequest(String uri, HttpMethod method) {
+		return makeApiRequest(currentUser().getName(), uri, method);
+	}
 
     /*
      * Make an http request to the API with path variables and description in the body.
      */
     @SuppressWarnings("lgtm[java/xss]")
-    @Override
-    public ResponseEntity<String> makeApiRequestWithBody(String currentUser, String uri, String data,
+    public ResponseEntity<String> makeApiRequestWithBody(String user, String uri, String data,
             HttpMethod method) {
 
         HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.set(CURRENT_USER, currentUser);
+        httpHeaders.set(KEY_CURRENT_USER, user);
         HttpEntity<String> httpEntity = new HttpEntity<>(data, httpHeaders);
 
-        RestTemplate restTemplate =
-                new RestTemplateBuilder().errorHandler(new RestTemplateResponseErrorHandler()).build();
+		RestTemplate restTemplate = restTemplate();
         return restTemplate.exchange(uri, method, httpEntity, String.class);
     }
+
+	public ResponseEntity<String> makeApiRequestWithBody(String uri, String data, HttpMethod method) {
+		return makeApiRequestWithBody(currentUser().getName(), uri, data, method);
+	}
+
+	private User currentUser() {
+		return userContextService.getCurrentUser();
+	}
+
+	private RestTemplate restTemplate() {
+		return new RestTemplateBuilder()
+				.errorHandler(new RestTemplateResponseErrorHandler())
+				.build();
+	}
 }
