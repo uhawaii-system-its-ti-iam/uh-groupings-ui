1fbbb5611d78361f329d556e24467907b3f3865d
Cody McCodePants <cody@mccodepants.com>
2022-01-21 11:47:03 -1000

    Removed HttpRequestService Interface

--- src/main/java/edu/hawaii/its/api/service/HttpRequestService.java
+++ src/main/java/edu/hawaii/its/api/service/HttpRequestService.java
@@ -1,12 +1,52 @@
 package edu.hawaii.its.api.service;
 
+import edu.hawaii.its.api.controller.RestTemplateResponseErrorHandler;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-public interface HttpRequestService {
+@Service("httpRequestService")
+public class HttpRequestService implements HttpRequestService {
 
-    ResponseEntity<String> makeApiRequest(String currentUser, String uri, HttpMethod method);
+    @Value("${groupings.api.current_user}")
+    private String CURRENT_USER;
 
-    ResponseEntity<String> makeApiRequestWithBody(String currentUser, String uri, String data, HttpMethod method);
+    /*
+     * Make an http request to the API with path variables.
+     */
+    @SuppressWarnings("lgtm[java/xss]")
+    @Override
+    public ResponseEntity<String> makeApiRequest(String currentUser, String uri, HttpMethod method) {
 
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set(CURRENT_USER, currentUser);
+        HttpEntity<String> httpEntity = new HttpEntity<>(httpHeaders);
+
+        RestTemplate restTemplate =
+                new RestTemplateBuilder().errorHandler(new RestTemplateResponseErrorHandler()).build();
+        return restTemplate.exchange(uri, method, httpEntity, String.class);
+    }
+
+    /*
+     * Make an http request to the API with path variables and description in the body.
+     */
+    @SuppressWarnings("lgtm[java/xss]")
+    @Override
+    public ResponseEntity<String> makeApiRequestWithBody(String currentUser, String uri, String data,
+            HttpMethod method) {
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set(CURRENT_USER, currentUser);
+        HttpEntity<String> httpEntity = new HttpEntity<>(data, httpHeaders);
+
+        RestTemplate restTemplate =
+                new RestTemplateBuilder().errorHandler(new RestTemplateResponseErrorHandler()).build();
+        return restTemplate.exchange(uri, method, httpEntity, String.class);
+    }
 }
