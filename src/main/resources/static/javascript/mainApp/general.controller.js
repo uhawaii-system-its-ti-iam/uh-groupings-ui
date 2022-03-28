820eb20e404543c0e08bf39a5e4c17387cbbfca9
Gavin Peng <blackbullet54321@gmail.com>
2022-03-28 12:43:35 -1000

    Add sanitizer function and test

--- src/main/resources/static/javascript/mainApp/general.controller.js
+++ src/main/resources/static/javascript/mainApp/general.controller.js
@@ -630,6 +630,15 @@
             }
         };
 
+        // Checks that a users name matches the pattern of either a valid uid or a uhUuid
+        $scope.sanitizer = (name) => {
+            const regexPattern = new RegExp("^[_?a-z-?@?0-9]{3,64}$");
+            if (name != null && regexPattern.test(name)) {
+                const validInput = name.match(regexPattern);
+                return validInput.toString();
+            }
+        };
+        
         /**
          * Read a text file(.txt) from client side. The file should consist of
          * a list of UH usernames or ids separated by newline characters. This
@@ -644,7 +653,15 @@
             let reader = new FileReader();
             reader.onload = function (e) {
                 let str = e.target.result;
-                $scope.manageMembers = (str.split(/[\r\n]+/).join(" ")).slice();
+                $scope.manageMembers = str.split(/[\r\n]+/);
+                let sanitizedFile = [];
+                for (const members of $scope.manageMembers) {
+                    let sanitizedName = $scope.sanitizer(members);
+                    if (sanitizedName != null) { 
+                        sanitizedFile.push(sanitizedName);
+                    }
+                }
+                $scope.manageMembers = sanitizedFile.join(" ");
                 $scope.addMembers($scope.listName);
             };
             reader.readAsText(file);
