cd35cc3118233fc0552b58d8aef939bfb9b55a54
Kobey Arai <45755166+Kobeyarai808@users.noreply.github.com>
2022-03-31 17:43:03 -1000

    Fixed Codacy Issues

--- src/main/resources/static/javascript/mainApp/app.js
+++ src/main/resources/static/javascript/mainApp/app.js
@@ -16,14 +16,14 @@ UHGroupingsApp.controller("MembershipJsController",function($scope){
 
 });
 
-UHGroupingsApp.directive('fileModel', [ '$parse', function($parse) {
+UHGroupingsApp.directive("fileModel", [ "$parse", function($parse) {
   return {
-    restrict: 'A',
-    link: function(scope, element, attrs) {
+    restrict: "A",
+    link(scope, element, attrs) {
       var model = $parse(attrs.fileModel);
       var modelSetter = model.assign;
 
-      element.bind('change', function() {
+      element.bind("change", function() {
         scope.$apply(function() {
           modelSetter(scope, element[0].files[0]);
         });
