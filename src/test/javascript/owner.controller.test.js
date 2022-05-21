a9dcb2ac0cdca6fc46b10e1cd2d4b73f4fa0bf7c
Will <williamliang808@gmail.com>
2022-05-20 15:32:45 -1000

    Adds init test

--- src/test/javascript/owner.controller.test.js
+++ src/test/javascript/owner.controller.test.js
@@ -7,18 +7,31 @@ describe("OwnerController", function () {
     let controller;
     let httpBackend;
     let BASE_URL;
+    let gs;
 
-    beforeEach(inject(function ($rootScope, $controller, _BASE_URL_, _$httpBackend_) {
+    beforeEach(inject(function ($rootScope, $controller, _BASE_URL_, _$httpBackend_, groupingsService) {
         scope = $rootScope.$new();
         controller = $controller("OwnerJsController", {
             $scope: scope
         });
         httpBackend = _$httpBackend_;
         BASE_URL = _BASE_URL_;
+        gs = groupingsService;
     }));
 
     it("should define the owner controller", function () {
         expect(controller).toBeDefined();
     });
 
-});
+    describe("init", () => {
+        it("should set loading to true can call groupingsService", () => {
+            scope.loading = false;
+            spyOn(gs, "getGroupingsOwned").and.callThrough();
+
+            scope.init();
+
+            expect(scope.loading).toBeTrue();
+            expect(gs.getGroupingsOwned).toHaveBeenCalled();
+        });
+    })
+})
