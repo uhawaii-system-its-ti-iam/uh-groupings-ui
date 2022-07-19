cbc9a26da75a0cea83bcc7c0849bb14a51faba91
Gavin Peng <blackbullet54321@gmail.com>
2022-07-19 11:55:00 -1000

    Add import back

--- src/main/java/edu/hawaii/its/groupings/configuration/SpringBootWebApplication.java
+++ src/main/java/edu/hawaii/its/groupings/configuration/SpringBootWebApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
