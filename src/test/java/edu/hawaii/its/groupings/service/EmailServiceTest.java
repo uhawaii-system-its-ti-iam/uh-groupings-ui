1862aa69332cbbbeaa1157930ab7e2795f39b460
Gavin Peng <blackbullet54321@gmail.com>
2022-08-03 13:41:35 -1000

    add testing for when a user sends a problem as a feedback type with no stacktrace

--- src/test/java/edu/hawaii/its/groupings/service/EmailServiceTest.java
+++ src/test/java/edu/hawaii/its/groupings/service/EmailServiceTest.java
@@ -5,6 +5,9 @@ import edu.hawaii.its.groupings.type.Feedback;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import edu.internet2.middleware.grouperClientExt.util.JsonUtils;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mail.MailException;
 import org.springframework.mail.MailSendException;
@@ -83,6 +86,21 @@ public class EmailServiceTest {
         assertThat(messageSent.getText(), containsString("Some problem happened."));
         assertThat(messageSent.getText(), containsString("Stack Trace:"));
         assertThat(messageSent.getText(), containsString("ArrayIndexOutOfBoundsException"));
+    }
+
+    @Test
+    public void sendFeedbackWithOutExceptionMessage() {
+        Feedback feedback = createBaseFeedback();
+
+        emailService.send(feedback);
+        assertThat(wasSent, is(true));
+
+        assertThat(messageSent.getSubject(), containsString("problem"));
+        assertThat(messageSent.getText(), containsString("John Doe"));
+        assertThat(messageSent.getText(), containsString("jdoe@hawaii.edu"));
+        assertThat(messageSent.getText(), containsString("Some problem happened."));
+        assertThat(messageSent.getText(), containsString(""));
+        assertThat(messageSent.getText(), containsString(""));
     }
 
     @Test
