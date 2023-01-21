07e98cd52f8fd7ee6cc7807f049e5f47c0e88961
Kristyn-Mimura <97662067+Kristyn-Mimura@users.noreply.github.com>
2023-01-20 16:18:29 -1000

    Normalizes the properties files between the api and ui (#699)
     Makes the tooltip directive

--- src/main/resources/static/javascript/mainApp/app.directives.js
+++ src/main/resources/static/javascript/mainApp/app.directives.js
@@ -37,3 +37,30 @@ UHGroupingsApp.directive("tooltipOnTruncate", function () {
         }
     };
 });
+
+UHGroupingsApp.directive("tooltip", function () {
+    return {
+        restrict: "A",
+        scope: {
+            title: "@tooltip"
+        },
+        link(scope, element) {
+            element.attr("data-toggle","tooltip");
+            element.attr("data-original-title", scope.title);
+            element.attr("data-boundary", "window");
+            element.attr("data-delay", "{\"show\": 300}");
+
+            element.on("mouseover", function () {
+                $(element).tooltip("toggle");
+            });
+            element.on("mouseout", function () {
+                $(element).tooltip("dispose");
+            });
+
+        }
+
+        // element.attr("data-placement", attrs["tooltipPosition"]);
+        // this line will make the tooltip nonformatted apparently
+
+    };
+});
--- src/test/javascript/app.directives.test.js
+++ src/test/javascript/app.directives.test.js
@@ -1,5 +1,4 @@
 /* global inject */
-
 describe("Directives", () => {
 
     beforeEach(module("UHGroupingsApp"));
@@ -66,6 +65,44 @@ describe("Directives", () => {
             $(element).triggerSVGEvent("mouseover");
             expect($.fn.tooltip).not.toHaveBeenCalledWith("toggle");
             $(element).remove();
+        }));
+    });
+
+    describe("tooltip", () => {
+
+        beforeEach(function () {
+            $.fn.triggerSVGEvent = function (eventName) {
+                let event = new Event(eventName, {"bubbles": true, "cancelable": true});
+                this[0].dispatchEvent(event);
+                return $(this);
+            };
+        });
+
+        it("should check tooltip has all the attributes from the tooltip directive", inject(() => {
+            let text = "This is a tooltip";
+
+            let element = document.createElement("div");
+            element.style.width = "100px";
+            element.setAttribute("tooltip", text);
+            element.textContent = text;
+            document.body.appendChild(element);
+
+            element = compile(element)(scope);
+            scope.$digest();
+
+            expect(element.attr("tooltip")).toBe(text);
+            expect(element.attr("data-toggle")).toBe("tooltip");
+            expect(element.attr("data-boundary")).toBe("window");
+            expect(element.attr("data-original-title")).toBe(text);
+            expect(element.attr("data-delay")).toBe("{\"show\": 300}");
+
+            spyOn($.fn, "tooltip");
+            $(element).triggerSVGEvent("mouseover");
+            expect($.fn.tooltip).toHaveBeenCalledWith("toggle");
+            $(element).triggerSVGEvent("mouseout");
+            expect($.fn.tooltip).toHaveBeenCalledWith("dispose");
+            $(element).remove();
+
         }));
     });
 });
