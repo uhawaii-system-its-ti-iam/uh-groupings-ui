4c48d42a40db109c13d9dd0723551c2db47ab7a3
FeimeiChen <54688836+FeimeiChen@users.noreply.github.com>
2023-06-30 14:07:50 -1000

    Remove AppConfig class (#822)

--- src/main/java/edu/hawaii/its/groupings/configuration/SpringBootWebApplication.java
+++ src/main/java/edu/hawaii/its/groupings/configuration/SpringBootWebApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "edu.hawaii.its")
 public class SpringBootWebApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws Exception {
