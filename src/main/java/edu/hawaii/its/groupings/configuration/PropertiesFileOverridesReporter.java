c6e7ed30408301edc8f328527e85a5bf308cda7c
FeimeiChen <54688836+FeimeiChen@users.noreply.github.com>
2023-07-05 10:59:17 -1000

    Make PropertiesFileOverridesReporter Class

---
+++ src/main/java/edu/hawaii/its/groupings/configuration/PropertiesFileOverridesReporter.java
@@ -0,0 +1,23 @@
+package edu.hawaii.its.groupings.configuration;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class PropertiesFileOverridesReporter {
+
+    private static final Log logger = LogFactory.getLog(PropertiesFileOverridesReporter.class);
+    private final String flag;
+
+    public PropertiesFileOverridesReporter(@Value("${overrides.file.found:}") String flag) {
+        this.flag = flag;
+    }
+
+    @PostConstruct
+    public void checkOverridesStatus() {
+        logger.info("Overrides property file found: " + flag);
+    }
+}
