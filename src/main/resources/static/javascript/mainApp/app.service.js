33aa4e09eccd65c022aa128f1448df6a1c99fa29
Jordan Wong <42422209+JorWo@users.noreply.github.com>
2023-11-04 12:40:03 -1000

    Backport: Implement retry on error for loading a large grouping (#862)

--- src/main/resources/static/javascript/mainApp/app.service.js
+++ src/main/resources/static/javascript/mainApp/app.service.js
@@ -7,6 +7,7 @@
     UHGroupingsApp.factory("dataProvider", function ($http, $window, BASE_URL) {
 
         const timeLimit = 20000;
+        const maxRetries = 3;
 
         /**
          * Sets delay in milliseconds. Used with await in async functions.
@@ -109,6 +110,27 @@
                         pollData(response.data, callback, callError);
                     }, (response) => {
                         callError(response);
+                    });
+            },
+
+            /**
+             * Perform a POST request to the specified URL that retries on error with incremental delay.
+             * @param {string} url - the URL to perform the request on
+             * @param {any} data - the data to perform the request with
+             * @param {function} callback - the function to perform on a successful request (200)
+             * @param {function} callError - Execute if response returns as an error.
+             * @param {number} retries - the number of retries to attempt
+             */
+            loadDataWithBodyRetry(url, data, callback, callError, retries = maxRetries) {
+                $http.post(encodeURI(url), data)
+                    .then((response) => callback(response.data))
+                    .catch(async (response) => {
+                        if (retries <= 0) {
+                            callError(response);
+                            return;
+                        }
+                        await delay(2000 * Math.log(maxRetries / retries));
+                        this.loadDataWithBodyRetry(url, data, callback, callError, retries - 1);
                     });
             },
 
