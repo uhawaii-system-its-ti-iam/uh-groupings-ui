cacf3dc347af6076123ddaf349c32e9ab67d1ceb
Kristyn-Mimura <97662067+Kristyn-Mimura@users.noreply.github.com>
2024-01-05 13:47:02 -1000

    Remvoes injected principal (#889)

--- src/main/java/edu/hawaii/its/api/controller/ForTestingController.java
+++ src/main/java/edu/hawaii/its/api/controller/ForTestingController.java
@@ -1,7 +1,5 @@
 package edu.hawaii.its.api.controller;
 
-import java.security.Principal;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.hawaii.its.api.service.HttpRequestService;
-
+import edu.hawaii.its.groupings.access.UserContextService;
 @RestController
 @RequestMapping("/api/groupings/testing")
-public class GroupingsRestControllerForTesting {
+public class ForTestingController {
 
-    private static final Log logger = LogFactory.getLog(GroupingsRestControllerForTesting.class);
+    private static final Log logger = LogFactory.getLog(ForTestingController.class);
 
     @Value("${url.api.2.1.base}/testing")
     private String API_2_1_BASE;
@@ -26,10 +24,14 @@ public class GroupingsRestControllerForTesting {
     @Autowired
     private HttpRequestService httpRequestService;
 
+    @Autowired
+    private UserContextService userContextService;
+
     @GetMapping(value = "/exception")
-    public ResponseEntity<String> throwException(Principal principal) {
+    public ResponseEntity<String> throwException() {
         logger.info("Entered REST throwException...");
+        String currentUsername = userContextService.getCurrentUsername();
         String uri = String.format(API_2_1_BASE + "/exception");
-        return httpRequestService.makeApiRequest(principal.getName(), uri, HttpMethod.GET);
+        return httpRequestService.makeApiRequest(currentUsername, uri, HttpMethod.GET);
     }
 }
