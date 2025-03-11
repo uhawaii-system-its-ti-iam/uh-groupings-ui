cf81de60836cb0b7d126e63b8732244d171c091c
Ralph Jhon Ramos <22ralph.ramos@gmail.com>
2025-03-11 13:42:26 -1000

    Attempting to solve merge conflicts

--- src/test/java/edu/hawaii/its/groupings/controller/ErrorControllerAdviceTest.java
+++ src/test/java/edu/hawaii/its/groupings/controller/ErrorControllerAdviceTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import edu.hawaii.its.api.type.GroupingsServiceResultException;
