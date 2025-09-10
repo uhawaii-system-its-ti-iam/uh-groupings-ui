509c80abe53ab27e3654a3424ebfb8501266f366
Mikhail Shkaralevich <mkaralevich8@gmail.com>
2025-09-09 15:13:45 -1000

    Implement JWT

--- src/main/java/edu/hawaii/its/api/service/HttpRequestService.java
+++ src/main/java/edu/hawaii/its/api/service/HttpRequestService.java
@@ -5,17 +5,27 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import edu.hawaii.its.groupings.access.User;
+import edu.hawaii.its.groupings.access.UserContextService;
+import edu.hawaii.its.groupings.service.JwtService;
 
 @Service("httpRequestService")
 public class HttpRequestService {
 
+    private final JwtService jwtService;
+    private final UserContextService userContextService;
     private final WebClient webClient;
     @Value("${groupings.api.current_user}")
     private String CURRENT_USER;
 
-    public HttpRequestService() {
+    public HttpRequestService(JwtService jwtService, UserContextService userContextService) {
+        this.userContextService = userContextService;
+        this.jwtService = jwtService;
         webClient = WebClient.builder()
                 .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(-1))
                 .build();
@@ -25,9 +35,11 @@ public class HttpRequestService {
      * Make a http request to the API with path variables.
      */
     public ResponseEntity<String> makeApiRequest(String currentUser, String uri, HttpMethod method) {
+        User user = userContextService.getCurrentUser();
+        String jwt = jwtService.generateToken(user);
         return webClient.method(method)
                 .uri(uri)
-                .header(CURRENT_USER, currentUser)
+                .header("Authorization", "Bearer " + jwt)
                 .retrieve()
                 .toEntity(String.class)
                 .block();
---
+++ src/main/java/edu/hawaii/its/groupings/service/JwtService.java
@@ -0,0 +1,38 @@
+package edu.hawaii.its.groupings.service;
+
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+
+    public String generateToken(UserDetails userDetails) {
+        // Short-lived token as it is for a single server-to-server hop.
+        long expirationTime = 1000 * 60 * 5; // 5 min
+
+        return Jwts.builder()
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+}
