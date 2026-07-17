7d6a2aad5d3fedf01b9421b3d66483c0475690c0
zhongyu <zhongyu@hawaii.edu>
2026-07-17 12:01:10 -1000

    Fixed Endpoints

--- src/main/resources/static/javascript/mainApp/groupings.service.js
+++ src/main/resources/static/javascript/mainApp/groupings.service.js
@@ -445,7 +445,7 @@
              * Checks if the owner of a grouping is the sole owner
              */
             getNumberOfDirectOwners(path, onSuccess, onError) {
-                let endpoint = BASE_URL + path + "/owners/count";
+                let endpoint = BASE_URL + "members/" + path + "/owners/count";
                 dataProvider.loadData(endpoint, onSuccess, onError);
             },
 
